3f2f52bdf9d5a972e76f9629644f22479dec6a19
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2000-09-24 23:25:41 +0000

    NotationView widget and interface description.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@156 cf6a83e7-f017-4305-b0b3-678bde24f230

---
+++ gui/notationview.cpp
@@ -0,0 +1,569 @@
+/***************************************************************************
+                          notationview.cpp  -  description
+                             -------------------
+    begin                : Fri Sep 22 2000
+    copyright            : (C) 2000 by Guillaume Laurent, Chris Cannam, Rich Bown
+    email                : glaurent@telegraph-road.org, cannam@all-day-breakfast.com, bownie@bownie.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <sys/times.h>
+
+#include <qcanvas.h>
+
+#include <kmessagebox.h>
+#include <kmenubar.h>
+#include <klocale.h>
+#include <kconfig.h>
+#include <kaction.h>
+#include <kstdaction.h>
+#include <kapp.h>
+
+#include "rosegardenguidoc.h"
+#include "notationview.h"
+
+#include "qcanvasspritegroupable.h"
+#include "qcanvaslinegroupable.h"
+#include "staff.h"
+#include "notepixmapfactory.h"
+#include "qcanvassimplesprite.h"
+#include "quantizer.h"
+#include "resource.h"
+
+#include "rosedebug.h"
+
+NotationView::NotationView(RosegardenGUIDoc* doc)
+    : KMainWindow(0),
+      m_config(kapp->config()),
+      m_document(doc),
+      m_canvasView(new QCanvasView(new QCanvas(width() * 2,
+                                               height() * 2),
+                                   this)),
+      m_mainStaff(new Staff(canvas())),
+      m_currentStaff(m_mainStaff),
+      m_movingItem(0),
+      m_draggingItem(false),
+      m_hlayout(0),
+      m_vlayout(0)
+{
+
+    kdDebug(KDEBUG_AREA) << "NotationView ctor" << endl;
+
+    setupActions();
+
+    setBackgroundMode(PaletteBase);
+
+    setCentralWidget(m_canvasView);
+
+    if (doc) {
+        setCaption(doc->getTitle());
+    }
+    
+    //#define TEST_CANVAS
+#ifndef TEST_CANVAS
+
+    RosegardenGUIDoc* doc = getDocument();
+    
+    if (doc) {
+
+        EventList &allEvents(doc->getEvents());
+
+        m_notationElements = ViewElementsManager::notationElementList(allEvents.begin(),
+                                                                      allEvents.end());
+
+        m_mainStaff->move(20, 15);
+        m_mainStaff->show();
+
+        m_vlayout = new NotationVLayout(*m_mainStaff);
+        m_hlayout = new NotationHLayout((Staff::noteWidth + 2) * 4, // this shouldn't be constant
+                                        4, // 4 beats per bar
+                                        40);
+
+        if (!applyLayout()) {
+
+            // Show all elements in the staff
+            showElements(m_notationElements->begin(), m_notationElements->end(), m_mainStaff);
+
+        } else {
+            KMessageBox::sorry(0, "Couldn't apply layout");
+        }
+
+    } else {
+        kdDebug(KDEBUG_AREA) << "NotationView ctor : getDocument() returned 0" << endl;
+        KMessageBox::sorry(0, "No document");
+    }
+    
+#else
+    test();
+#endif
+}
+
+NotationView::~NotationView()
+{
+    
+    // Delete canvas items.
+    QCanvasItemList allItems = canvas()->allItems();
+    QCanvasItemList::Iterator it;
+
+    for(it = allItems.begin(); it != allItems.end(); ++it)
+        delete *it;
+
+    delete canvas();
+    delete m_hlayout;
+    delete m_vlayout;
+}
+
+void
+NotationView::saveOptions()
+{	
+    m_config->setGroup("Notation Options");
+    m_config->writeEntry("Geometry", size());
+    m_config->writeEntry("Show Toolbar", toolBar()->isVisible());
+    m_config->writeEntry("Show Statusbar",statusBar()->isVisible());
+    m_config->writeEntry("ToolBarPos", (int) toolBar()->barPos());
+}
+
+void
+NotationView::readOptions()
+{
+    m_config->setGroup("Notation Options");
+	
+    QSize size(m_config->readSizeEntry("Geometry"));
+
+    if(!size.isEmpty()) {
+        resize(size);
+    }
+}
+
+void
+NotationView::setupActions()
+{
+    // setup Notes menu
+    NotePixmapFactory npf;
+    QIconSet icon(npf.makeNotePixmap(Whole));
+    
+    new KAction(i18n("Whole"), icon, 0, this,
+                SLOT(slotWhole()), actionCollection(), "whole_note" );
+
+    icon = QIconSet(npf.makeNotePixmap(Half));
+    new KAction(i18n("Half"), icon, 0, this,
+                SLOT(slotHalf()), actionCollection(), "half" );
+
+    icon = QIconSet(npf.makeNotePixmap(Quarter));
+    new KAction(i18n("Quarter"), icon, 0, this,
+                SLOT(slotQuarter()), actionCollection(), "quarter" );
+
+    icon = QIconSet(npf.makeNotePixmap(Eighth));
+    new KAction(i18n("8th"), icon, 0, this,
+                SLOT(slot8th()), actionCollection(), "8th" );
+
+    icon = QIconSet(npf.makeNotePixmap(Sixteenth));
+    new KAction(i18n("16th"), icon, 0, this,
+                SLOT(slot16th()), actionCollection(), "16th" );
+
+    icon = QIconSet(npf.makeNotePixmap(ThirtySecond));
+    new KAction(i18n("32nd"), icon, 0, this,
+                SLOT(slot32nd()), actionCollection(), "32nd" );
+
+    icon = QIconSet(npf.makeNotePixmap(SixtyFourth));
+    new KAction(i18n("64th"), icon, 0, this,
+                SLOT(slot64th()), actionCollection(), "64th" );
+    
+
+    // setup edit menu
+    KStdAction::undo     (this, SLOT(slotEditUndo()),       actionCollection());
+    KStdAction::redo     (this, SLOT(slotEditRedo()),       actionCollection());
+    KStdAction::cut      (this, SLOT(slotEditCut()),        actionCollection());
+    KStdAction::copy     (this, SLOT(slotEditCopy()),       actionCollection());
+    KStdAction::paste    (this, SLOT(slotEditPaste()),      actionCollection());
+
+    // setup Settings menu
+    KStdAction::showToolbar  (this, SLOT(slotToggleToolBar()),   actionCollection());
+    KStdAction::showStatusbar(this, SLOT(slotToggleStatusBar()), actionCollection());
+
+    KStdAction::saveOptions(this, SLOT(save_options()), actionCollection());
+    KStdAction::preferences(this, SLOT(customize()),    actionCollection());
+
+    KStdAction::keyBindings      (this, SLOT(editKeys()),     actionCollection());
+    KStdAction::configureToolbars(this, SLOT(editToolbars()), actionCollection());
+
+    createGUI("notation.rc");
+}
+
+
+void
+NotationView::contentsMouseReleaseEvent (QMouseEvent *e)
+{
+    m_draggingItem = false;
+    canvas()->update();
+}
+
+void
+NotationView::contentsMouseMoveEvent (QMouseEvent *e)
+{
+    if(m_draggingItem) {
+        m_movingItem->move(e->x(), e->y());
+        canvas()->update();
+    }
+}
+
+void
+NotationView::contentsMousePressEvent (QMouseEvent *e)
+{
+    QCanvasItemList itemList = canvas()->collisions(e->pos());
+
+    if(itemList.isEmpty()) { // click was not on an item
+        return;
+    }
+
+    QCanvasItem *item = itemList.first();
+
+    QCanvasGroupableItem *gitem;
+
+    if((gitem = dynamic_cast<QCanvasGroupableItem*>(item))) {
+        kdDebug(KDEBUG_AREA) << "Groupable item" << endl;
+        QCanvasItemGroup *t = gitem->group();
+
+        if(t->active())
+            m_movingItem = t;
+        else {
+            kdDebug(KDEBUG_AREA) << "Unmoveable groupable item" << endl;
+            m_movingItem = 0; // this is not a moveable item
+            return;
+        }
+    } else {
+        m_movingItem = item;
+    }
+
+    m_draggingItem = true;
+    m_movingItem->move(e->x(), e->y());
+    canvas()->update();
+}
+
+
+
+void
+NotationView::perfTest()
+{
+    // perf test - add many many notes
+    clock_t st, et;
+    struct tms spare;
+    st = times(&spare);
+
+
+    cout << "Adding 1000 notes" << endl;
+    setUpdatesEnabled(false);
+
+    QCanvasPixmapArray *notePixmap = new QCanvasPixmapArray("pixmaps/note-bodyfilled.xpm");
+
+    for(unsigned int x = 0; x < 1000; ++x) {
+        for(unsigned int y = 0; y < 100; ++y) {
+
+
+            QCanvasSprite *clef = new QCanvasSprite(notePixmap, canvas());
+
+            clef->move(x * 10, y * 10);
+        }
+    }
+    setUpdatesEnabled(true);
+
+    cout << "Done adding 1000 notes" << endl;
+    et = times(&spare);
+
+    cout << (et-st)*10 << "ms" << endl;
+}
+
+void
+NotationView::test()
+{
+    //     QCanvasEllipse *t = new QCanvasEllipse(10, 10, canvas());
+    //     t->setX(50);
+    //     t->setY(50);
+
+    //     QBrush brush(blue);
+    //     t->setBrush(brush);
+
+    Staff *staff = new Staff(canvas());
+    staff->move(20, 15);
+		
+    staff = new Staff(canvas());
+    staff->move(20, 130);
+		
+    // Add some notes
+
+    QCanvasPixmapArray *notePixmap = new QCanvasPixmapArray("pixmaps/note-bodyfilled.xpm");
+
+    for(unsigned int i = 0; i <= 17; ++i) {
+        QCanvasSprite *note = new QCanvasSprite(notePixmap, canvas());
+        note->move(20,14);
+        note->moveBy(40 + i * 20, staff->pitchYOffset(i));
+    }
+
+    ChordPixmapFactory npf(*staff);
+
+    for(unsigned int j = 0; j < 100; ++j) {
+
+        for(unsigned int i = 0; i < 7; ++i) {
+
+
+            QPixmap note(npf.makeNotePixmap(Note(i), true, true));
+
+            QCanvasSimpleSprite *noteSprite = new QCanvasSimpleSprite(&note,
+                                                                      canvas());
+
+            noteSprite->move(50 + (i+j) * 20 , 100);
+
+        }
+    }
+    
+#if 0
+
+    for(unsigned int i = 0; i < 7; ++i) {
+
+
+        QPixmap note(npf.makeNotePixmap(i, true, false));
+
+        QCanvasSprite *noteSprite = new QCanvasSimpleSpriteSprite(&note,
+                                                                  canvas());
+
+        noteSprite->move(50 + i * 20, 150);
+
+    }
+#endif
+
+    chordpitches pitches;
+    pitches.push_back(6); // something wrong here - pitches aren't in the right order
+    pitches.push_back(4);
+    pitches.push_back(0);
+
+    QPixmap chord(npf.makeChordPixmap(pitches, Note(6), true, false));
+
+    QCanvasSprite *chordSprite = new QCanvasSimpleSprite(&chord, canvas());
+
+    chordSprite->move(50, 50);
+   
+}
+
+bool
+NotationView::showElements(NotationElementList::iterator from,
+                                NotationElementList::iterator to)
+{
+    return showElements(from, to, 0, 0);
+}
+
+bool
+NotationView::showElements(NotationElementList::iterator from,
+                                NotationElementList::iterator to,
+                                QCanvasItem *item)
+{
+    return showElements(from, to, item->x(), item->y());
+}
+
+bool
+NotationView::showElements(NotationElementList::iterator from,
+                                NotationElementList::iterator to,
+                                double dxoffset, double dyoffset)
+{
+    static ChordPixmapFactory npf(*m_mainStaff);
+
+    for(NotationElementList::iterator it = from; it != to; ++it) {
+        
+        Note note = Note((*it)->event()->get<Int>("Notation::NoteType"));
+        
+        QCanvasPixmap notePixmap(npf.makeNotePixmap(note, true, true));
+        QCanvasSimpleSprite *noteSprite = new QCanvasSimpleSprite(&notePixmap,
+                                                                  canvas());
+        noteSprite->move(dxoffset + (*it)->x(),
+                         dyoffset + (*it)->y());
+        noteSprite->show();
+        
+    }
+
+
+    // Display bars
+    const NotationHLayout::barpositions& barPositions(m_hlayout->barPositions());
+
+    for(NotationHLayout::barpositions::const_iterator it = barPositions.begin();
+        it != barPositions.end(); ++it) {
+
+        unsigned int barPos = *it;
+
+        kdDebug(KDEBUG_AREA) << "Adding bar at pos " << barPos << endl;
+
+        QCanvasLineGroupable* barLine = new QCanvasLineGroupable(canvas(), m_currentStaff);
+
+        barLine->setPoints(0, Staff::linesOffset,
+                           0, m_currentStaff->barLineHeight() + Staff::linesOffset);
+        barLine->move(barPos + dxoffset,
+                      dyoffset);
+        barLine->show();
+    }
+    
+
+    return true;
+}
+
+
+bool
+NotationView::applyLayout()
+{
+    bool rch = applyHorizontalLayout();
+    bool rcv = applyVerticalLayout();
+
+    return rch && rcv;
+}
+
+
+bool
+NotationView::applyHorizontalLayout()
+{
+    if (!m_hlayout) {
+        KMessageBox::error(0, "No Horizontal Layout engine");
+        return false;
+    }
+
+    for (NotationElementList::iterator i = m_notationElements->begin();
+         i != m_notationElements->end(); ++i)
+        (*m_hlayout)(*i);
+
+    return m_hlayout->status();
+}
+
+
+bool 
+NotationView::applyVerticalLayout()
+{
+    if (!m_vlayout) {
+        KMessageBox::error(0, "No Vertical Layout engine");
+        return false;
+    }
+
+    for_each(m_notationElements->begin(), m_notationElements->end(), *m_vlayout);
+    
+    return m_vlayout->status();
+}
+
+//////////////////////////////////////////////////////////////////////
+//                    Slots
+//////////////////////////////////////////////////////////////////////
+
+void NotationView::slotEditUndo()
+{
+    slotStatusMsg(i18n("Undo..."));
+
+    slotStatusMsg(i18n(IDS_STATUS_DEFAULT));
+}
+
+void NotationView::slotEditRedo()
+{
+    slotStatusMsg(i18n("Redo..."));
+
+    slotStatusMsg(i18n(IDS_STATUS_DEFAULT));
+}
+
+void NotationView::slotEditCut()
+{
+    slotStatusMsg(i18n("Cutting selection..."));
+
+    slotStatusMsg(i18n(IDS_STATUS_DEFAULT));
+}
+
+void NotationView::slotEditCopy()
+{
+    slotStatusMsg(i18n("Copying selection to clipboard..."));
+
+    slotStatusMsg(i18n(IDS_STATUS_DEFAULT));
+}
+
+void NotationView::slotEditPaste()
+{
+    slotStatusMsg(i18n("Inserting clipboard contents..."));
+
+    slotStatusMsg(i18n(IDS_STATUS_DEFAULT));
+}
+
+void NotationView::slotToggleToolBar()
+{
+    slotStatusMsg(i18n("Toggle the toolbar..."));
+
+    if (toolBar()->isVisible())
+        toolBar()->hide();
+    else
+        toolBar()->show();
+
+    slotStatusMsg(i18n(IDS_STATUS_DEFAULT));
+}
+
+void NotationView::slotToggleStatusBar()
+{
+    slotStatusMsg(i18n("Toggle the statusbar..."));
+
+    if (statusBar()->isVisible())
+        statusBar()->hide();
+    else
+        statusBar()->show();
+
+    slotStatusMsg(i18n(IDS_STATUS_DEFAULT));
+}
+
+
+void NotationView::slotStatusMsg(const QString &text)
+{
+    ///////////////////////////////////////////////////////////////////
+    // change status message permanently
+    statusBar()->clear();
+    statusBar()->changeItem(text, ID_STATUS_MSG);
+}
+
+
+void NotationView::slotStatusHelpMsg(const QString &text)
+{
+    ///////////////////////////////////////////////////////////////////
+    // change status message of whole statusbar temporary (text, msec)
+    statusBar()->message(text, 2000);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+void NotationView::slotWhole()
+{
+    kdDebug(KDEBUG_AREA) << "NotationView::slotWhole()\n";
+}
+
+void NotationView::slotHalf()
+{
+    kdDebug(KDEBUG_AREA) << "NotationView::slotHalf()\n";
+}
+
+void NotationView::slotQuarter()
+{
+    kdDebug(KDEBUG_AREA) << "NotationView::slotQuarter()\n";
+}
+
+void NotationView::slot8th()
+{
+    kdDebug(KDEBUG_AREA) << "NotationView::slot8th()\n";
+}
+
+void NotationView::slot16th()
+{
+    kdDebug(KDEBUG_AREA) << "NotationView::slot16th()\n";
+}
+
+void NotationView::slot32nd()
+{
+    kdDebug(KDEBUG_AREA) << "NotationView::slot32nd()\n";
+}
+
+void NotationView::slot64th()
+{
+    kdDebug(KDEBUG_AREA) << "NotationView::slot64th()\n";
+}
---
+++ gui/notationview.h
@@ -0,0 +1,178 @@
+/***************************************************************************
+                          notationview.h  -  description
+                             -------------------
+    begin                : Fri Sep 22 2000
+    copyright            : (C) 2000 by Guillaume Laurent, Chris Cannam, Rich Bown
+    email                : glaurent@telegraph-road.org, cannam@all-day-breakfast.com, bownie@bownie.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef NOTATIONVIEW_H
+#define NOTATIONVIEW_H
+
+#include <kmainwindow.h>
+
+#include "notationelement.h"
+#include "viewelementsmanager.h"
+#include "notationhlayout.h"
+#include "notationvlayout.h"
+
+class QCanvasItem;
+class RosegardenGUIDoc;
+
+/**
+  *@author Guillaume Laurent, Chris Cannam, Rich Bown
+  */
+
+
+class NotationView : public KMainWindow
+{
+    Q_OBJECT
+public:
+
+    NotationView(RosegardenGUIDoc* doc);
+    ~NotationView();
+
+    const RosegardenGUIDoc *getDocument() const { return m_document; }
+    RosegardenGUIDoc *getDocument() { return m_document; }
+
+    /// draw all elements
+    virtual bool showElements(NotationElementList::iterator from,
+                              NotationElementList::iterator to);
+
+    /// same, with dx,dy offset
+    virtual bool showElements(NotationElementList::iterator from,
+                              NotationElementList::iterator to,
+                              double dxoffset, double dyoffset);
+
+    /// same, relative to the specified item
+    virtual bool showElements(NotationElementList::iterator from,
+                              NotationElementList::iterator to,
+                              QCanvasItem*);
+
+    /// Normally calls applyHorizontalLayout() then applyVerticalLayout()
+    virtual bool applyLayout();
+
+    /// Set the 'y'-coord on all doc elements - should be called before applyVerticalLayout()
+    virtual bool applyHorizontalLayout();
+
+    /// Set the 'x'-coord on all doc elements - should be called after applyHorizontalLayout()
+    virtual bool applyVerticalLayout();
+    
+    void setHorizontalLayoutEngine(NotationHLayout* e) { m_hlayout = e; }
+    void setVerticalLayoutEngine(NotationVLayout* e)   { m_vlayout = e; }
+
+    LayoutEngine* getHorizontalLayoutEngine() { return m_hlayout; }
+    LayoutEngine* getVerticalLayoutEngine()   { return m_vlayout; }
+
+    QCanvas* canvas() { return m_canvasView->canvas(); }
+
+public slots:
+
+    /** undo
+     */
+    void slotEditUndo();
+    /** redo
+     */
+    void slotEditRedo();
+    
+    /** put the marked text/object into the clipboard and remove
+     *	it from the document
+     */
+    void slotEditCut();
+
+    /** put the marked text/object into the clipboard
+     */
+    void slotEditCopy();
+
+    /** paste the clipboard into the document
+     */
+    void slotEditPaste();
+
+    /** toggles the toolbar
+     */
+    void slotToggleToolBar();
+
+    /** toggles the statusbar
+     */
+    void slotToggleStatusBar();
+
+    /** changes the statusbar contents for the standard label permanently, used to indicate current actions.
+     * @param text the text that is displayed in the statusbar
+     */
+    void slotStatusMsg(const QString &text);
+
+    /** changes the status message of the whole statusbar for two seconds, then restores the last status. This is used to display
+     * statusbar messages that give information about actions for toolbar icons and menuentries.
+     * @param text the text that is displayed in the statusbar
+     */
+    void slotStatusHelpMsg(const QString &text);
+
+
+    // note switch slots
+    void slotWhole();
+    void slotHalf();
+    void slotQuarter();
+    void slot8th();
+    void slot16th();
+    void slot32nd();
+    void slot64th();
+	
+protected:
+
+    /**
+     * save general Options like all bar positions and status as well
+     * as the geometry and the recent file list to the configuration
+     * file
+     */
+    void saveOptions();
+
+    /**
+     * read general Options again and initialize all variables like the recent file list
+     */
+    void readOptions();
+
+    /**
+     * create menus and toolbars
+     */
+    void setupActions();
+
+
+    /** Callback for a mouse button press event in the canvas */
+    virtual void contentsMousePressEvent (QMouseEvent *e);
+    /** Callback for a mouse button release event in the canvas */
+    virtual void contentsMouseReleaseEvent (QMouseEvent *e);
+    /** Callback for a mouse move event in the canvas */
+    virtual void contentsMouseMoveEvent (QMouseEvent *e);
+
+    void perfTest();
+    void test();
+
+    KConfig* m_config;
+
+    RosegardenGUIDoc* m_document;
+
+    QCanvasView* m_canvasView;
+
+    Staff* m_mainStaff;
+    Staff* m_currentStaff;
+
+    NotationElementList* m_notationElements;
+    
+    QCanvasItem* m_movingItem;
+    bool m_draggingItem;
+
+    NotationHLayout* m_hlayout;
+    NotationVLayout* m_vlayout;
+	
+};
+
+#endif
