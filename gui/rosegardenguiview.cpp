26da3a304cddf52962cc70763677b9d44b955089
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2000-09-24 23:24:06 +0000

    Moved to notation stuff into notationview.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@154 cf6a83e7-f017-4305-b0b3-678bde24f230

--- gui/rosegardenguiview.cpp
+++ gui/rosegardenguiview.cpp
@@ -15,93 +15,49 @@
  *                                                                         *
  ***************************************************************************/
 
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <sys/times.h>
-
 // include files for Qt
 #include <qprinter.h>
-#include <qpainter.h>
-#include <qcanvas.h>
 
 // KDE includes
 #include <kdebug.h>
-#include <kmessagebox.h>
 
 // application specific includes
 #include "rosegardenguiview.h"
 #include "rosegardenguidoc.h"
 #include "rosegardengui.h"
-#include "qcanvasspritegroupable.h"
-#include "qcanvaslinegroupable.h"
-#include "staff.h"
-#include "notepixmapfactory.h"
-#include "qcanvassimplesprite.h"
-#include "quantizer.h"
+
+#include "notationview.h"
 
 RosegardenGUIView::RosegardenGUIView(QWidget *parent, const char *name)
-    : QCanvasView(new QCanvas(parent->width() * 2, parent->height() * 2),
-                  parent, name),
-    m_mainStaff(new Staff(canvas())),
-    m_currentStaff(m_mainStaff),
-    m_movingItem(0),
-    m_draggingItem(false),
-    m_hlayout(0),
-    m_vlayout(0)
+    : QVBox(parent)
 {
-
-    kdDebug(KDEBUG_AREA) << "RosegardenGUIView ctor" << endl;
-
-    setBackgroundMode(PaletteBase);
-    
-    //#define TEST_CANVAS
-#ifndef TEST_CANVAS
-
-    EventList &allEvents(getDocument()->getEvents());
-
-    m_notationElements = ViewElementsManager::notationElementList(allEvents.begin(),
-                                                                  allEvents.end());
-
-    m_mainStaff->move(20, 15);
-    m_mainStaff->show();
-
-    m_vlayout = new NotationVLayout(*m_mainStaff);
-    m_hlayout = new NotationHLayout((Staff::noteWidth + 2) * 4, // this shouldn't be constant
-                                    4, // 4 beats per bar
-                                    40);
-
-    if (!applyLayout()) {
-
-        // Show all elements in the staff
-        showElements(m_notationElements->begin(), m_notationElements->end(), m_mainStaff);
-
-    } else {
-        KMessageBox::sorry(0, "Couldn't apply layout");
-    }
-#else
-    test();
-#endif
+    new QLabel("environment view here", this);
+    NotationView *n = new NotationView(getDocument());
+    n->show();
 }
+
 
 RosegardenGUIView::~RosegardenGUIView()
 {
-    // Delete canvas items.
-    QCanvasItemList allItems = canvas()->allItems();
-    QCanvasItemList::Iterator it;
-
-    for(it = allItems.begin(); it != allItems.end(); ++it)
-        delete *it;
-
-    delete canvas();
-    delete m_hlayout;
-    delete m_vlayout;
 }
 
-RosegardenGUIDoc *RosegardenGUIView::getDocument() const
+RosegardenGUIDoc*
+RosegardenGUIView::getDocument() const
 {
-    RosegardenGUIApp *theApp=(RosegardenGUIApp *) parentWidget();
+    QWidget *t = parentWidget();
+    
+    if (!t) {
+        kdDebug(KDEBUG_AREA) << "CRITICAL ERROR : RosegardenGUIView::getDocument() : widget parent is 0\n";
+        return 0;
+    }
 
+    RosegardenGUIApp *theApp = dynamic_cast<RosegardenGUIApp*>(t);
+    
+    if (!theApp) {
+        kdDebug(KDEBUG_AREA) << "CRITICAL ERROR : RosegardenGUIView::getDocument() : widget parent is of the wrong type\n";
+        return 0;
+    }
+    
     return theApp->getDocument();
 }
 
@@ -114,257 +70,3 @@ void RosegardenGUIView::print(QPrinter *pPrinter)
 
     printpainter.end();
 }
-
-
-void
-RosegardenGUIView::contentsMouseReleaseEvent (QMouseEvent *e)
-{
-    m_draggingItem = false;
-    canvas()->update();
-}
-
-void
-RosegardenGUIView::contentsMouseMoveEvent (QMouseEvent *e)
-{
-    if(m_draggingItem) {
-        m_movingItem->move(e->x(), e->y());
-        canvas()->update();
-    }
-}
-
-void
-RosegardenGUIView::contentsMousePressEvent (QMouseEvent *e)
-{
-    QCanvasItemList itemList = canvas()->collisions(e->pos());
-
-    if(itemList.isEmpty()) { // click was not on an item
-        return;
-    }
-
-    QCanvasItem *item = itemList.first();
-
-    QCanvasGroupableItem *gitem;
-
-    if((gitem = dynamic_cast<QCanvasGroupableItem*>(item))) {
-        kdDebug(KDEBUG_AREA) << "Groupable item" << endl;
-        QCanvasItemGroup *t = gitem->group();
-
-        if(t->active())
-            m_movingItem = t;
-        else {
-            kdDebug(KDEBUG_AREA) << "Unmoveable groupable item" << endl;
-            m_movingItem = 0; // this is not a moveable item
-            return;
-        }
-    } else {
-        m_movingItem = item;
-    }
-
-    m_draggingItem = true;
-    m_movingItem->move(e->x(), e->y());
-    canvas()->update();
-}
-
-
-
-void
-RosegardenGUIView::perfTest()
-{
-    // perf test - add many many notes
-    clock_t st, et;
-    struct tms spare;
-    st = times(&spare);
-
-
-    cout << "Adding 1000 notes" << endl;
-    setUpdatesEnabled(false);
-
-    QCanvasPixmapArray *notePixmap = new QCanvasPixmapArray("pixmaps/note-bodyfilled.xpm");
-
-    for(unsigned int x = 0; x < 1000; ++x) {
-        for(unsigned int y = 0; y < 100; ++y) {
-
-
-            QCanvasSprite *clef = new QCanvasSprite(notePixmap, canvas());
-
-            clef->move(x * 10, y * 10);
-        }
-    }
-    setUpdatesEnabled(true);
-
-    cout << "Done adding 1000 notes" << endl;
-    et = times(&spare);
-
-    cout << (et-st)*10 << "ms" << endl;
-}
-
-void
-RosegardenGUIView::test()
-{
-    //     QCanvasEllipse *t = new QCanvasEllipse(10, 10, canvas());
-    //     t->setX(50);
-    //     t->setY(50);
-
-    //     QBrush brush(blue);
-    //     t->setBrush(brush);
-
-    Staff *staff = new Staff(canvas());
-    staff->move(20, 15);
-		
-    staff = new Staff(canvas());
-    staff->move(20, 130);
-		
-    // Add some notes
-
-    QCanvasPixmapArray *notePixmap = new QCanvasPixmapArray("pixmaps/note-bodyfilled.xpm");
-
-    for(unsigned int i = 0; i <= 17; ++i) {
-        QCanvasSprite *note = new QCanvasSprite(notePixmap, canvas());
-        note->move(20,14);
-        note->moveBy(40 + i * 20, staff->pitchYOffset(i));
-    }
-
-    ChordPixmapFactory npf(*staff);
-
-    for(unsigned int j = 0; j < 100; ++j) {
-
-        for(unsigned int i = 0; i < 7; ++i) {
-
-
-            QPixmap note(npf.makeNotePixmap(Note(i), true, true));
-
-            QCanvasSimpleSprite *noteSprite = new QCanvasSimpleSprite(&note,
-                                                                      canvas());
-
-            noteSprite->move(50 + (i+j) * 20 , 100);
-
-        }
-    }
-    
-#if 0
-
-    for(unsigned int i = 0; i < 7; ++i) {
-
-
-        QPixmap note(npf.makeNotePixmap(i, true, false));
-
-        QCanvasSprite *noteSprite = new QCanvasSimpleSpriteSprite(&note,
-                                                                  canvas());
-
-        noteSprite->move(50 + i * 20, 150);
-
-    }
-#endif
-
-    chordpitches pitches;
-    pitches.push_back(6); // something wrong here - pitches aren't in the right order
-    pitches.push_back(4);
-    pitches.push_back(0);
-
-    QPixmap chord(npf.makeChordPixmap(pitches, Note(6), true, false));
-
-    QCanvasSprite *chordSprite = new QCanvasSimpleSprite(&chord, canvas());
-
-    chordSprite->move(50, 50);
-   
-}
-
-bool
-RosegardenGUIView::showElements(NotationElementList::iterator from,
-                                NotationElementList::iterator to)
-{
-    return showElements(from, to, 0, 0);
-}
-
-bool
-RosegardenGUIView::showElements(NotationElementList::iterator from,
-                                NotationElementList::iterator to,
-                                QCanvasItem *item)
-{
-    return showElements(from, to, item->x(), item->y());
-}
-
-bool
-RosegardenGUIView::showElements(NotationElementList::iterator from,
-                                NotationElementList::iterator to,
-                                double dxoffset, double dyoffset)
-{
-    static ChordPixmapFactory npf(*m_mainStaff);
-
-    for(NotationElementList::iterator it = from; it != to; ++it) {
-        
-        Note note = Note((*it)->event()->get<Int>("Notation::NoteType"));
-        
-        QCanvasPixmap notePixmap(npf.makeNotePixmap(note, true, true));
-        QCanvasSimpleSprite *noteSprite = new QCanvasSimpleSprite(&notePixmap,
-                                                                  canvas());
-        noteSprite->move(dxoffset + (*it)->x(),
-                         dyoffset + (*it)->y());
-        noteSprite->show();
-        
-    }
-
-
-    // Display bars
-    const NotationHLayout::barpositions& barPositions(m_hlayout->barPositions());
-
-    for(NotationHLayout::barpositions::const_iterator it = barPositions.begin();
-        it != barPositions.end(); ++it) {
-
-        unsigned int barPos = *it;
-
-        kdDebug(KDEBUG_AREA) << "Adding bar at pos " << barPos << endl;
-
-        QCanvasLineGroupable* barLine = new QCanvasLineGroupable(canvas(), m_currentStaff);
-
-        barLine->setPoints(0, Staff::linesOffset,
-                           0, m_currentStaff->barLineHeight() + Staff::linesOffset);
-        barLine->move(barPos + dxoffset,
-                      dyoffset);
-        barLine->show();
-    }
-    
-
-    return true;
-}
-
-
-bool
-RosegardenGUIView::applyLayout()
-{
-    bool rch = applyHorizontalLayout();
-    bool rcv = applyVerticalLayout();
-
-    return rch && rcv;
-}
-
-
-bool
-RosegardenGUIView::applyHorizontalLayout()
-{
-    if (!m_hlayout) {
-        KMessageBox::error(0, "No Horizontal Layout engine");
-        return false;
-    }
-
-    for (NotationElementList::iterator i = m_notationElements->begin();
-         i != m_notationElements->end(); ++i)
-        (*m_hlayout)(*i);
-
-    return m_hlayout->status();
-}
-
-
-bool 
-RosegardenGUIView::applyVerticalLayout()
-{
-    if (!m_vlayout) {
-        KMessageBox::error(0, "No Vertical Layout engine");
-        return false;
-    }
-
-    for_each(m_notationElements->begin(), m_notationElements->end(), *m_vlayout);
-    
-    return m_vlayout->status();
-}
-
