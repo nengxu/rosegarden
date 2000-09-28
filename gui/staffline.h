1175a5d66b2e7baa7c0127757bf90921dc01cfc9
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2000-09-28 00:28:14 +0000

    Added associated pitch data member.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@171 cf6a83e7-f017-4305-b0b3-678bde24f230

--- gui/staffline.h
+++ gui/staffline.h
@@ -21,16 +21,29 @@
 #include <qcanvaslinegroupable.h>
 
 /**
- * A Staff line - this is just because we need them to be of a specific type
- * in NotationView::contentsMousePressEvent()
-  *@author Guillaume Laurent, Chris Cannam, Rich Bown
+ * A staff line as a canvas item
+ *
+ *@author Guillaume Laurent, Chris Cannam, Rich Bown
  */
 class StaffLine : public QCanvasLineGroupable
 {
 public:
     StaffLine(QCanvas *c, QCanvasItemGroup *g)
-        : QCanvasLineGroupable(c, g)
+        : QCanvasLineGroupable(c, g), m_pitch(0)
     {}
+
+    /**
+     * sets the pitch this staff line corresponds to (e.g. the pitch
+     * of a natural note which would be on this line)
+     */
+    void setAssociatedPitch(unsigned int p) { m_pitch = p; }
+
+    /// returns the pitch this staff line corresponds to
+    unsigned int associatedPitch() const { return m_pitch; }
+
+protected:
+
+    unsigned int m_pitch;
 };
 
 #endif
