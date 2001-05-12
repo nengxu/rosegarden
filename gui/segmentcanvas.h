c936632ee3d452ef23da03ae7fd78eeeb1206d57
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2001-05-12 15:17:52 +0000

    Code indentation.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@295 cf6a83e7-f017-4305-b0b3-678bde24f230

--- gui/segmentcanvas.h
+++ gui/segmentcanvas.h
@@ -51,8 +51,8 @@ class TracksCanvas : public QCanvasView  {
    Q_OBJECT
 public:
     TracksCanvas(int gridH, int gridV,
-               QCanvas&,
-               QWidget* parent=0, const char* name=0, WFlags f=0);
+                 QCanvas&,
+                 QWidget* parent=0, const char* name=0, WFlags f=0);
     ~TracksCanvas();
 
     void clear();
