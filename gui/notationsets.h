1e79dd29714af13247d018fcbc43959d46b56f7f
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2001-08-11 10:58:24 +0000

    Doc formatting.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@711 cf6a83e7-f017-4305-b0b3-678bde24f230

--- gui/notationsets.h
+++ gui/notationsets.h
@@ -29,33 +29,33 @@
 class Staff;
 
 
-// A "notation set" is an object made up from a contiguous set of
-// elements from an element list.  Examples of notation sets include
-// the Chord and BeamedGroup.
-
-// To construct a set requires (at least) a NotationElementList
-// reference plus an iterator into that list.  The constructor (or
-// more precisely the initialise() method called by the constructor)
-// then scans the surrounding area of the list for the maximal set of
-// contiguous elements before and after the passed-in iterator such
-// that all elements are in the same set (i.e. Chord, BeamedGroup etc)
-// as the one that the passed-in iterator pointed to.
-
-// The extents of the set within the list can then be discovered via
-// getInitialElement() and getFinalElement().  If the iterator passed
-// in to the constructor was at end() or did not point to an element
-// that could be a member of this kind of set, getInitialElement()
-// will return end(); if the passed-in iterator pointed to the only
-// member of this set, getInitialElement() and getFinalElement() will
-// be equal.
-
-// These classes are not intended to be stored anywhere; all they
-// contain is iterators into the main element list, and those might
-// not persist.  Instead you should create these on-the-fly when you
-// want, for example, to consider a note as part of a chord; and then
-// you should let them expire when you've finished with them.
-
-
+/**
+* A "notation set" is an object made up from a contiguous set of
+* elements from an element list.  Examples of notation sets include
+* the Chord and BeamedGroup.
+*
+* To construct a set requires (at least) a NotationElementList
+* reference plus an iterator into that list.  The constructor (or
+* more precisely the initialise() method called by the constructor)
+* then scans the surrounding area of the list for the maximal set of
+* contiguous elements before and after the passed-in iterator such
+* that all elements are in the same set (i.e. Chord, BeamedGroup etc)
+* as the one that the passed-in iterator pointed to.
+*
+* The extents of the set within the list can then be discovered via
+* getInitialElement() and getFinalElement().  If the iterator passed
+* in to the constructor was at end() or did not point to an element
+* that could be a member of this kind of set, getInitialElement()
+* will return end(); if the passed-in iterator pointed to the only
+* member of this set, getInitialElement() and getFinalElement() will
+* be equal.
+*
+* These classes are not intended to be stored anywhere; all they
+* contain is iterators into the main element list, and those might
+* not persist.  Instead you should create these on-the-fly when you
+* want, for example, to consider a note as part of a chord; and then
+* you should let them expire when you've finished with them.
+*/
 class NotationSet // abstract base
 {
 public:
@@ -63,22 +63,26 @@ public:
 
     virtual ~NotationSet() { }
 
-    // getInitialElement() returns end() if there are no elements in
-    // the set.  getInitialElement() == getFinalElement() if there is
-    // only one element in the set
+    /**
+     * getInitialElement() returns end() if there are no elements in
+     * the set.  getInitialElement() == getFinalElement() if there is
+     * only one element in the set
+     */
     virtual NELIterator getInitialElement() const  { return m_initial;  }
     virtual NELIterator getFinalElement() const    { return m_final;    }
 
-    // only return note elements; will return end() if there are none
+    /// only return note elements; will return end() if there are none
     virtual NELIterator getInitialNote() const;
     virtual NELIterator getFinalNote() const;
 
-    // only elements with duration > 0 are candidates for shortest and
-    // longest; these will return end() if there are no such elements
+    /**
+     * only elements with duration > 0 are candidates for shortest and
+     * longest; these will return end() if there are no such elements
+     */
     virtual NELIterator getLongestElement() const  { return m_longest;  }
     virtual NELIterator getShortestElement() const { return m_shortest; }
 
-    // these will return end() if there are no note elements in the set
+    /// these will return end() if there are no note elements in the set
     virtual NELIterator getHighestNote() const     { return m_highest;  }
     virtual NELIterator getLowestNote() const      { return m_lowest;   }
 
@@ -102,13 +106,14 @@ private:
 };
 
 
-// Chord is subclassed from a vector of notation list iterators; this
-// vector contains iterators pointing at all the notes in the chord,
-// in ascending order of pitch.  (You can also track through all the
-// notes in the chord by iterating from getInitialElement() to
-// getFinalElement(), but this will only get them in the order in
-// which they appear in the original notation list.)
-
+/**
+* Chord is subclassed from a vector of notation list iterators; this
+* vector contains iterators pointing at all the notes in the chord,
+* in ascending order of pitch.  (You can also track through all the
+* notes in the chord by iterating from getInitialElement() to
+* getFinalElement(), but this will only get them in the order in
+* which they appear in the original notation list.)
+*/
 class Chord : public NotationSet,
               public std::vector<NotationElementList::iterator>
 {
@@ -144,15 +149,16 @@ class NotationGroup : public NotationSet
 public:
     enum Type { Beamed, Tupled, Grace };
 
-    // If the iterator passed in to the constructor points at an
-    // element with a GroupNo property, the resulting Group will
-    // contain iterators pointing to it and all surrounding elements
-    // that have the same GroupNo, sorted in ascending order of
-    // absolute time.  If no other surrounding elements have the same
-    // GroupNo as this one, we will have size 1; if this iterator
-    // doesn't point to an element with a GroupNo at all, we will have
-    // size 0.
-    
+    /**
+     * If the iterator passed in to the constructor points at an
+     * element with a GroupNo property, the resulting Group will
+     * contain iterators pointing to it and all surrounding elements
+     * that have the same GroupNo, sorted in ascending order of
+     * absolute time.  If no other surrounding elements have the same
+     * GroupNo as this one, we will have size 1; if this iterator
+     * doesn't point to an element with a GroupNo at all, we will have
+     * size 0.
+     */
     NotationGroup(const NotationElementList &nel, NELIterator elementInGroup,
                   const Rosegarden::Clef &clef, const Rosegarden::Key &key);
 
@@ -160,8 +166,10 @@ public:
 
     Type getGroupType() const { return m_type; }
 
-    // Writes beam data into each note in the group.  Notes'
-    // layout x coordinates must already have been set.
+    /**
+     * Writes beam data into each note in the group.  Notes'
+     * layout x coordinates must already have been set.
+     */
     void applyBeam(Staff &);
 
 protected:
