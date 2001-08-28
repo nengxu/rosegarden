ac5af134545dfd1a3780d1e3a4ffb7dfdb2b3934
cannam <cannam@cf6a83e7-f017-4305-b0b3-678bde24f230>
2001-08-28 21:38:46 +0000

    small tweaks
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@807 cf6a83e7-f017-4305-b0b3-678bde24f230

--- gui/layoutengine.cpp
+++ gui/layoutengine.cpp
@@ -23,8 +23,7 @@
 // I woke up on the last day of the year
 // with the sudden realisation
 // that people have brought terrible ills upon themselves by
-// trying to cover the earth with
-// fields in shapes such as squares
+// trying to cover the earth with fields in shapes such as squares
 // which don't tesselate when mapped onto curved surfaces.
 // War and famine would cease, if only we could all
 // move at once onto a system of triangles.
