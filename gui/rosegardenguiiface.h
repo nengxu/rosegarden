b3b69745cd715ffeab677f340d47e48b53286d8a
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2001-09-04 23:10:56 +0000

    Exposed importMIDIFile().
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@863 cf6a83e7-f017-4305-b0b3-678bde24f230

--- gui/rosegardenguiiface.h
+++ gui/rosegardenguiiface.h
@@ -11,6 +11,7 @@ public:
 k_dcop:
     virtual int  openFile(const QString &file) = 0;
     virtual int  importRG21File(const QString &file) = 0;
+    virtual int  importMIDIFile(const QString &file) = 0;
     virtual void fileNew()                       = 0;
     virtual void fileSave()                      = 0;
     virtual void fileClose()                     = 0;
