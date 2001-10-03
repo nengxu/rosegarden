110f2c90820c93dd0ca5aaa17671cd81e1a28456
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2001-10-03 09:11:38 +0000

    CLK_TCK appears to be deprecated.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@981 cf6a83e7-f017-4305-b0b3-678bde24f230

--- gui/rosedebug.h
+++ gui/rosedebug.h
@@ -62,7 +62,7 @@ operator<<(kndbgstream &s, const Rosegarden::Track&) { return s; }
 #define START_TIMING \
   struct tms dbgSpare; clock_t dbgStart = times(&dbgSpare);
 #define ELAPSED_TIME \
-  ((times(&dbgSpare) - dbgStart) * 1000 / CLK_TCK)
+  ((times(&dbgSpare) - dbgStart) * 1000 / CLOCKS_PER_SEC)
 #define PRINT_ELAPSED(n) \
   std::cout << n << ": " << ELAPSED_TIME << "ms elapsed" << std::endl;
 
