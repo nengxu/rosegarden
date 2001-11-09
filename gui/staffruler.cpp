84636de43d965947d2d4a9b3f5c53cc6ff16e817
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2001-11-09 23:37:02 +0000

    Added staffruler.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@1126 cf6a83e7-f017-4305-b0b3-678bde24f230

---
+++ gui/staffruler.cpp
@@ -0,0 +1,144 @@
+#include <staffruler.h>
+
+
+//////////////////////////////////////////////////////////////////////
+
+StaffRuler::StepElement::StepElement(QCanvasLine* l, QCanvasText* t)
+    : stepLine(l), label(t)
+{
+}
+
+StaffRuler::StepElement::StepElement(const StepElement& e)
+    : stepLine(e.stepLine), label(e.label),
+      subSteps(e.subSteps)
+{
+}
+
+StaffRuler::StepElement& StaffRuler::StepElement::operator=(const StaffRuler::StepElement& e)
+{
+    stepLine = e.stepLine;
+    label = e.label;
+    subSteps = e.subSteps;
+
+    return *this;
+}
+
+
+void StaffRuler::StepElement::addSubStep(QCanvasLine* l)
+{
+    subSteps.push_back(l);
+}
+
+
+StaffRuler::StaffRuler(int xPos, int yPos, QCanvas* c)
+    : m_canvas(c),
+      m_xPos(xPos),
+      m_yPos(yPos),
+      m_stepLineHeight(10),
+      m_subStepLineHeight(5),
+      m_mainLine(new QCanvasLine(m_canvas)),
+      m_cursorLine(new QCanvasLine(m_canvas))
+{
+    QCanvasRectangle *rulerBackground = new QCanvasRectangle(0, 0,
+                                                             m_canvas->width(), m_yPos, 
+                                                             m_canvas);
+    QColor bgColor(105, 170, 228);
+    
+    rulerBackground->setBrush(bgColor);
+    rulerBackground->setPen(bgColor);
+    rulerBackground->setZ(-1);
+    rulerBackground->show();
+
+    m_mainLine->setPoints(0, m_yPos, m_canvas->width(), m_yPos);
+    
+    m_cursorLine->setPoints(0, 0, 0, m_canvas->height());
+    
+    m_mainLine->show();
+    m_cursorLine->show();
+}
+
+void StaffRuler::clearSteps()
+{
+    m_steps.clear();
+
+    // TODO: recycle items instead
+    for (unsigned int i = 0; i < m_stepElements.size(); ++i) {
+        StepElement& stepEl = m_stepElements[i];
+        delete stepEl.stepLine;
+        delete stepEl.label;
+        for (unsigned int j = 0; j < stepEl.subSteps.size(); ++j)
+            delete stepEl.subSteps[j];
+    }
+
+    m_stepElements.clear();
+
+}
+
+void StaffRuler::addStep(double stepPos, unsigned short subSteps)
+{
+    m_steps.push_back(StepDef(stepPos, subSteps));
+}
+
+void StaffRuler::update()
+{
+    // TODO: perhaps recycle instead
+
+    for (unsigned int i = 0; i < m_steps.size() - 1; ++i) {
+         makeStep(i, m_steps[i].first, m_steps[i + 1].first, m_steps[i].second);
+    }
+
+//     m_mainLine->setPoints
+// 	(m_xPos, m_yPos,
+// 	 m_xPos + int(m_steps[m_steps.size() - 1].first) + 10, m_yPos);
+
+}
+
+void StaffRuler::makeStep(int stepValue,
+                          double stepPos, double nextStepPos,
+                          unsigned short nbSubsteps)
+{
+    // Make step line
+    //
+    QCanvasLine* stepLine = new QCanvasLine(m_canvas);
+    
+    stepLine->setPoints(int(stepPos) + m_xPos, m_yPos,
+                        int(stepPos) + m_xPos, m_yPos - m_stepLineHeight);
+    
+    stepLine->show();
+
+    // Make label
+    //
+    QString labelText;
+    labelText.setNum(stepValue);
+
+    QCanvasText* label = new QCanvasText(labelText, m_canvas);
+    label->setX(stepPos + m_xPos);
+    label->setY(m_yPos + 4);
+    label->setTextFlags(Qt::AlignHCenter);
+
+    // Prepare StepElement
+    StepElement stepEl(stepLine, label);
+    
+
+    // Make substeps
+    //
+    double incr = (nextStepPos - stepPos) / nbSubsteps;
+
+    for (double subStepPos = (stepPos + incr);
+         subStepPos <= (nextStepPos - incr);
+         subStepPos += incr) {
+
+        QCanvasLine* subStep = new QCanvasLine(m_canvas);
+    
+        subStep->setPoints(int(subStepPos) + m_xPos, m_yPos,
+                           int(subStepPos) + m_xPos, m_yPos - m_subStepLineHeight);
+        subStep->show();
+
+        stepEl.addSubStep(subStep);
+    }
+
+    label->show();
+
+    m_stepElements.push_back(stepEl);
+}
+
---
+++ gui/staffruler.h
@@ -0,0 +1,86 @@
+// -*- c-basic-offset: 4 -*-
+
+/*
+    Rosegarden-4 v0.1
+    A sequencer and musical notation editor.
+
+    This program is Copyright 2000-2001
+        Guillaume Laurent   <glaurent@telegraph-road.org>,
+        Chris Cannam        <cannam@all-day-breakfast.com>,
+        Richard Bown        <bownie@bownie.com>
+
+    The moral right of the authors to claim authorship of this work
+    has been asserted.
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version.  See the file
+    COPYING included with this distribution for more information.
+*/
+
+#ifndef STAFFRULER_H
+#define STAFFRULER_H
+
+#include <vector>
+#include <qcanvas.h>
+
+/**
+ * The StaffRuler is a ruler shown above the staff in the Notation
+ * window.
+ *
+ * It also deals with the position cursor
+ */
+class StaffRuler
+{
+public:
+    typedef std::pair<double, unsigned short> StepDef;
+    typedef std::vector<StepDef> Steps;
+
+    StaffRuler(int xPos, int yPos, QCanvas*);
+
+    void clearSteps();
+    void addStep(double stepPos, unsigned short subSteps);
+
+    /// Re-create all the steps and substeps
+    void update();
+
+    void setCursorPosition(unsigned int pos) { m_cursorLine->setX(pos); }
+    unsigned int getCursorPosition() const   { return int(m_cursorLine->x()); }
+    
+
+protected:
+    struct StepElement
+    {
+        StepElement(QCanvasLine* l, QCanvasText* t);
+        StepElement(const StepElement&);
+        StepElement& operator=(const StepElement& e);
+
+        void addSubStep(QCanvasLine*);
+
+        QCanvasLine* stepLine;
+        QCanvasText* label;
+        std::vector<QCanvasLine*> subSteps;
+    };
+    
+    void makeStep(int stepValue,
+                  double pos, double nextStepPos,
+                  unsigned short nbSubsteps);
+
+    QCanvas* m_canvas;
+
+    int m_xPos; // mainLine X pos
+    int m_yPos; // mainLine Y pos
+
+    int m_stepLineHeight,
+        m_subStepLineHeight;
+
+    QCanvasLine* m_mainLine;
+    QCanvasLine* m_cursorLine;
+
+    Steps m_steps;
+    
+    std::vector<StepElement> m_stepElements;
+};
+
+#endif
