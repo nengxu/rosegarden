d19df4cd1ffaf3a2e114b36bdbb837b5ddc06947
cannam <cannam@cf6a83e7-f017-4305-b0b3-678bde24f230>
2001-11-15 16:10:30 +0000

    ...
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@1170 cf6a83e7-f017-4305-b0b3-678bde24f230

---
+++ base/BaseProperties.C
@@ -0,0 +1,58 @@
+/*
+    Rosegarden-4 v0.1
+    A sequencer and musical notation editor.
+
+    This program is Copyright 2000-2001
+        Guillaume Laurent   <glaurent@telegraph-road.org>,
+        Chris Cannam        <cannam@all-day-breakfast.com>,
+        Richard Bown        <bownie@bownie.com>
+
+    The moral right of the authors to claim authorship of this work
+    has been asserted.
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version.  See the file
+    COPYING included with this distribution for more information.
+*/
+
+#include "BaseProperties.h"
+
+namespace Rosegarden
+{
+
+namespace BaseProperties
+{
+
+// Some of the most basic property names are defined in individual
+// classes in NotationTypes.h -- those are the ones that are used to
+// store the value of a clef/key/timesig event, whereas things like
+// notes have their values calculated from the duration property
+
+const PropertyName NOTE_TYPE            = "NoteType";
+const PropertyName NOTE_DOTS            = "NoteDots";
+
+const PropertyName TIED_BACKWARD	= "TiedBackward";
+const PropertyName TIED_FORWARD		= "TiedForward";
+
+// Note that these should be non-persistent properties, because
+// they're the same for each event in a group and so the code that
+// writes out XML converts them to group properties instead of
+// writing them as explicit properties of the events:
+
+const PropertyName BEAMED_GROUP_ID               = "BGroupId";
+const PropertyName BEAMED_GROUP_TYPE		 = "BGroupType";
+const PropertyName BEAMED_GROUP_TUPLED_LENGTH	 = "BGroupTupledLength";
+const PropertyName BEAMED_GROUP_TUPLED_COUNT	 = "BGroupTupledCount";
+const PropertyName BEAMED_GROUP_UNTUPLED_LENGTH	 = "BGroupUntupledLength";
+
+// This one is persistent, though, because it obviously isn't the same
+// for each event in a group:
+
+const PropertyName TUPLET_NOMINAL_DURATION	 = "TupletNominalDuration";
+
+}
+
+}
+
---
+++ base/BaseProperties.h
@@ -0,0 +1,50 @@
+/*
+    Rosegarden-4 v0.1
+    A sequencer and musical notation editor.
+
+    This program is Copyright 2000-2001
+        Guillaume Laurent   <glaurent@telegraph-road.org>,
+        Chris Cannam        <cannam@all-day-breakfast.com>,
+        Richard Bown        <bownie@bownie.com>
+
+    The moral right of the authors to claim authorship of this work
+    has been asserted.
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version.  See the file
+    COPYING included with this distribution for more information.
+*/
+
+#ifndef _BASE_PROPERTIES_H_
+#define _BASE_PROPERTIES_H_
+
+#include "PropertyName.h"
+
+namespace Rosegarden
+{
+
+namespace BaseProperties
+{
+
+extern const PropertyName NOTE_TYPE;
+extern const PropertyName NOTE_DOTS;
+
+extern const PropertyName TIED_BACKWARD;
+extern const PropertyName TIED_FORWARD;
+
+extern const PropertyName BEAMED_GROUP_ID;
+extern const PropertyName BEAMED_GROUP_TYPE;
+extern const PropertyName BEAMED_GROUP_TUPLED_LENGTH;
+extern const PropertyName BEAMED_GROUP_TUPLED_COUNT;
+extern const PropertyName BEAMED_GROUP_UNTUPLED_LENGTH;
+
+extern const PropertyName TUPLET_NOMINAL_DURATION;
+
+}
+
+}
+
+#endif
+
