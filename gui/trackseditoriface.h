6535fb2cbaa1ea23789345a9c17e40f52a91a948
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2002-01-04 19:03:39 +0000

    Added missing copyright banners.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@1371 cf6a83e7-f017-4305-b0b3-678bde24f230

--- gui/trackseditoriface.h
+++ gui/trackseditoriface.h
@@ -1,4 +1,24 @@
 // -*- c-basic-offset: 4 -*-
+
+/*
+    Rosegarden-4 v0.1
+    A sequencer and musical notation editor.
+
+    This program is Copyright 2000-2002
+        Guillaume Laurent   <glaurent@telegraph-road.org>,
+        Chris Cannam        <cannam@all-day-breakfast.com>,
+        Richard Bown        <bownie@bownie.com>
+
+    The moral right of the authors to claim authorship of this work
+    has been asserted.
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version.  See the file
+    COPYING included with this distribution for more information.
+*/
+
 #ifndef TRACKEDITORIFACE_H
 #define TRACKEDITORIFACE_H
 
