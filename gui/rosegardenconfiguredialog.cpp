0aea334a625e8fbd045323c35381c0fd9bc8965e
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2002-05-12 22:00:03 +0000

    Replaced confg dialog by something more KDE-ish, which should scale better. Not fully functionnal yet.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@2375 cf6a83e7-f017-4305-b0b3-678bde24f230

--- gui/rosegardenconfiguredialog.cpp
+++ gui/rosegardenconfiguredialog.cpp
@@ -19,12 +19,20 @@
     COPYING included with this distribution for more information.
 */
 
-#include <qconnect.h>
+/*
+ * Code borrowed from KDE KMail : configuredialog*.h
+ * Copyright (C) 2000 The KMail Development Team
+ */
+
 #include <qspinbox.h>
-#include <qpushbutton.h>
 #include <qslider.h>
-#include <qlabel.h>
 #include <qcombobox.h>
+#include <qlayout.h>
+#include <qtabwidget.h>
+#include <qlabel.h>
+
+#include <klocale.h>
+#include <kiconloader.h>
 
 #include "rosestrings.h"
 #include "rosegardenconfiguredialog.h"
@@ -36,69 +44,173 @@
 namespace Rosegarden
 {
 
+//
+//
+// basic ConfigurationPage (inherit pages from this)
+//
+//
+
+TabbedConfigurationPage::TabbedConfigurationPage(QWidget *parent,
+                                                 const char *name)
+  : ConfigurationPage(parent, name)
+{
+  QVBoxLayout *vlay = new QVBoxLayout(this, 0, KDialog::spacingHint());
+  m_tabWidget = new QTabWidget(this);
+  vlay->addWidget(m_tabWidget);
+}
+
+void TabbedConfigurationPage::addTab(QWidget *tab, const QString &title)
+{
+  m_tabWidget->addTab(tab, title);
+}
+
+//------------------------------------------------------------
+
+class GeneralConfigurationPage : public TabbedConfigurationPage
+{
+public:
+    GeneralConfigurationPage(RosegardenGUIDoc *doc,
+                             QWidget *parent=0, const char *name=0);
+
+    static QString iconLabel() { return i18n("General"); }
+    static QString title() { return i18n("General Configuration"); }
+
+    int getCountInSpin()    { return m_countIn->value(); }
+    int getDblClickClient() { return m_client->currentItem(); }
+    
+protected:
+    QComboBox* m_client;
+    QSpinBox* m_countIn;
+};
+
+GeneralConfigurationPage::GeneralConfigurationPage(RosegardenGUIDoc *doc,
+                                                   QWidget *parent, const char *name)
+    : TabbedConfigurationPage(parent, name),
+      m_client(0),
+      m_countIn(0)
+{
+    Rosegarden::Composition &comp = doc->getComposition();
+    Rosegarden::Configuration &config = doc->getConfiguration();
+
+    QFrame *frame = new QFrame(m_tabWidget);
+    QGridLayout *layout = new QGridLayout(frame, 2, 2,
+                                          10, 5);
+
+    layout->addWidget(new QLabel(i18n("Double click on segment opens..."), frame), 0, 0);
+    layout->addWidget(new QLabel(i18n("Number of count-in bars when recording"), frame), 1, 0);
+
+    m_client = new QComboBox(frame);
+    m_client->insertItem(i18n("Notation"));
+    m_client->insertItem(i18n("Matrix"));
+    m_client->setCurrentItem(config.getDoubleClickClient());
+
+    layout->addWidget(m_client, 0, 1);
+
+    m_countIn = new QSpinBox(frame);
+    m_countIn->setValue(comp.getCountInBars());
+    m_countIn->setMaxValue(10);
+    m_countIn->setMinValue(0);
+
+    layout->addWidget(m_countIn, 1, 1);
+
+    addTab(frame, i18n("General"));
+}
+
+
+class PlaybackConfigurationPage : public TabbedConfigurationPage
+{
+public:
+    PlaybackConfigurationPage(RosegardenGUIDoc *doc,
+                              QWidget *parent=0, const char *name=0);
+
+    static QString iconLabel() { return i18n("Playback"); }
+    static QString title()     { return i18n("Sequencer and Playback"); }
+
+    int getReadAheadValue() { return m_readAhead->value(); }
+    int getPlaybackValue()  { return m_playback->value(); }
+
+protected:
+    QSlider* m_readAhead;
+    QSlider* m_playback;
+};
+
+PlaybackConfigurationPage::PlaybackConfigurationPage(RosegardenGUIDoc *doc,
+                                                     QWidget *parent,
+                                                     const char *name)
+    : TabbedConfigurationPage(parent, name),
+      m_readAhead(0),
+      m_playback(0)
+{
+    Rosegarden::Composition &comp = doc->getComposition();
+    Rosegarden::Configuration &config = doc->getConfiguration();
+
+    QFrame *frame = new QFrame(m_tabWidget);
+    QGridLayout *layout = new QGridLayout(frame, 3, 2,
+                                          10, 5);
+
+    layout->addMultiCellWidget(new QLabel(i18n("Higher latency improves playback quality on slower systems\nbut reduces overall sequencer response."), frame),
+                               0, 0,
+                               0, 1);
+
+    layout->addWidget(new QLabel(i18n("Read ahead (in ms)"), frame), 1, 0);
+    layout->addWidget(new QLabel(i18n("Playback (in ms)"), frame), 2, 0);
+
+    m_readAhead = new QSlider(Horizontal, frame);
+    m_readAhead->setMinValue(20);
+    m_readAhead->setMaxValue(80);
+    m_readAhead->setValue(config.getReadAhead().usec / 1000);
+    m_readAhead->setTickmarks(QSlider::Below);
+    layout->addWidget(m_readAhead, 1, 1);
+
+    m_playback = new QSlider(Horizontal, frame);
+    m_playback->setMinValue(20);
+    m_playback->setMaxValue(500);
+    m_playback->setValue(config.getPlaybackLatency().usec / 1000);
+    m_playback->setTickmarks(QSlider::Below);
+    layout->addWidget(m_playback, 2, 1);
+
+    addTab(frame, i18n("Latency"));
+}
+
+
+
+//------------------------------------------------------------
+static inline QPixmap loadIcon( const char * name ) {
+  return KGlobal::instance()->iconLoader()
+    ->loadIcon( QString::fromLatin1(name), KIcon::NoGroup, KIcon::SizeMedium );
+}
+
 RosegardenConfigureDialog::RosegardenConfigureDialog(RosegardenGUIDoc *doc,
                                                      QWidget *parent,
                                                      const char *name):
-    RosegardenConfigure(parent, name, true), // modal
-    m_doc(doc)
+    KDialogBase(IconList, i18n("Configure"), Help|Apply|Ok|Cancel,
+                Ok, parent, name, true), // modal
+    m_doc(doc),
+    m_generalConfigurationPage(0),
+    m_playbackConfigurationPage(0)
 {
+  QWidget *page;
+  QVBoxLayout *vlay;
 
-    // buttons 
-    //
-    connect((QObject *)CancelButton, SIGNAL(released()),
-            this, SLOT(slotClose()));
+  // General Page
+  //
+  page = addPage(GeneralConfigurationPage::iconLabel(),
+                 GeneralConfigurationPage::title(),
+                 loadIcon(GeneralConfigurationPage::iconName()));
+  vlay = new QVBoxLayout(page, 0, spacingHint());
+  m_generalConfigurationPage = new GeneralConfigurationPage(m_doc, page);
+  vlay->addWidget(m_generalConfigurationPage);
+  m_generalConfigurationPage->setPageIndex(pageIndex(page));
 
-    connect((QObject *)ApplyButton, SIGNAL(released()),
-            this, SLOT(slotApply()));
-
-    connect((QObject *)OKButton, SIGNAL(released()),
-            this, SLOT(slotOK()));
-
-    // Ok, populate the fields
-    //
-    Rosegarden::Composition &comp = m_doc->getComposition();
-    Rosegarden::Configuration &config = m_doc->getConfiguration();
-
-    // count-in bars
-    //
-    CountInSpin->setValue(comp.getCountInBars());
-    CountInSpin->setMaxValue(10);
-    CountInSpin->setMinValue(0);
-
-    connect(CountInSpin, SIGNAL(valueChanged(int)),
-            this, SLOT(slotActivateApply()));
-
-    // latency sliders
-    connect((QObject *)PlaybackSlider, SIGNAL(valueChanged(int)),
-            (QObject *)PlaybackValue, SLOT(setNum(int)));
-
-    connect((QObject *)ReadAheadSlider, SIGNAL(valueChanged(int)),
-            (QObject *)ReadAheadValue, SLOT(setNum(int)));
-
-    connect((QObject *)PlaybackSlider, SIGNAL(valueChanged(int)),
-            this, SLOT(slotActivateApply()));
-
-    connect((QObject *)ReadAheadSlider, SIGNAL(valueChanged(int)),
-            this, SLOT(slotActivateApply()));
-
-    ReadAheadSlider->setMinValue(20);
-    ReadAheadSlider->setMaxValue(80);
-    ReadAheadSlider->setValue(config.getReadAhead().usec / 1000);
-
-    PlaybackSlider->setMinValue(20);
-    PlaybackSlider->setMaxValue(500);
-    PlaybackSlider->setValue(config.getPlaybackLatency().usec / 1000);
-
-    // set client combo
-    //
-    ClientCombo->setCurrentItem(config.getDoubleClickClient());
-
-    connect((QObject*)ClientCombo, SIGNAL(activated(int)),
-            this, SLOT(slotActivateApply()));
-
-    // Disable apply until something changes
-    //
-    ApplyButton->setDisabled(true);
+  // Playback Page
+  //
+  page = addPage(PlaybackConfigurationPage::iconLabel(),
+                 PlaybackConfigurationPage::title(),
+                 loadIcon(PlaybackConfigurationPage::iconName()));
+  vlay = new QVBoxLayout(page, 0, spacingHint());
+  m_playbackConfigurationPage = new PlaybackConfigurationPage(m_doc, page);
+  vlay->addWidget(m_playbackConfigurationPage);
+  m_playbackConfigurationPage->setPageIndex(pageIndex(page));
 
 }
 
@@ -118,27 +230,27 @@ RosegardenConfigureDialog::slotApply()
     Rosegarden::Composition &comp = m_doc->getComposition();
     Rosegarden::Configuration &config = m_doc->getConfiguration();
 
-    int countIn = CountInSpin->text().toInt();
+    int countIn = m_generalConfigurationPage->getCountInSpin();
     comp.setCountInBars(countIn);
 
-    int readAhead = ReadAheadValue->text().toInt();
+    int readAhead = m_playbackConfigurationPage->getReadAheadValue();
     config.setReadAhead((RealTime(0, (readAhead * 1000))));
 
-    int playback = PlaybackValue->text().toInt();
+    int playback = m_playbackConfigurationPage->getPlaybackValue();
     config.setPlaybackLatency((RealTime(0, (playback * 1000))));
 
-    int client = ClientCombo->currentItem();
+    int client = m_generalConfigurationPage->getDblClickClient();
     config.setDoubleClickClient(
             (Rosegarden::Configuration::DoubleClickClient)client);
 
-    ApplyButton->setDisabled(true);
+//     ApplyButton->setDisabled(true);
 }
 
 
 void
 RosegardenConfigureDialog::slotActivateApply()
 {
-    ApplyButton->setDisabled(false);
+//     ApplyButton->setDisabled(false);
 }
 
 void
--- gui/rosegardenconfiguredialog.h
+++ gui/rosegardenconfiguredialog.h
@@ -22,14 +22,96 @@
 #ifndef _ROSEGARDENCONFIGUREDIALOG_H_
 #define _ROSEGARDENCONFIGUREDIALOG_H_
 
-#include "rosegardenconfigure.h"
+//#include "rosegardenconfigure.h"
+
+#include <kdialogbase.h>
 
 class RosegardenGUIDoc;
 
 namespace Rosegarden
 {
 
-class RosegardenConfigureDialog : public RosegardenConfigure
+/**
+ * This class borrowed from KMail
+ * (c) 2000 The KMail Development Team
+ */
+class ConfigurationPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ConfigurationPage( QWidget * parent=0, const char * name=0 )
+        : QWidget( parent, name ) {}
+    ~ConfigurationPage() {};
+
+    /**
+     * Should set the page up (ie. read the setting from the @ref
+     * KConfig object into the widgets) after creating it in the
+     * constructor. Called from @ref ConfigureDialog.
+    */
+//     virtual void setup() = 0;
+
+    /**
+     * Called when the installation of a profile is
+     * requested. Reimplemenations of this method should do the
+     * equivalent of a @ref setup(), but with the given @ref KConfig
+     * object instead of kapp->config() and only for those entries that
+     * really have keys defined in the profile.
+     *
+     * The default implementation does nothing.
+     */
+    virtual void installProfile( KConfig * /*profile*/ ) {};
+
+    /**
+     * Should apply the changed settings (ie. read the settings from
+     * the widgets into the @ref KConfig object). Called from @ref
+     * ConfigureDialog.
+     */
+//     virtual void apply() = 0;
+
+    /**
+     * Should cleanup any temporaries after cancel. The default
+     * implementation does nothing. Called from @ref
+     * ConfigureDialog.
+     */
+    virtual void dismiss() {}
+
+    void setPageIndex( int aPageIndex ) { m_PageIndex = aPageIndex; }
+    int pageIndex() const { return m_PageIndex; }
+
+protected:
+    //--------------- Data members ---------------------------------
+
+    int m_PageIndex;
+};
+
+/**
+ * This class borrowed from KMail
+ * (c) 2000 The KMail Development Team
+ */
+class TabbedConfigurationPage : public ConfigurationPage
+{
+    Q_OBJECT
+
+public:
+    TabbedConfigurationPage(QWidget *parent=0, const char *name=0);
+
+    static QString iconName() { return "misc"; }
+    
+protected:
+    void addTab(QWidget *tab, const QString &title);
+
+    //--------------- Data members ---------------------------------
+
+    QTabWidget *m_tabWidget;
+
+};
+
+
+class GeneralConfigurationPage;
+class PlaybackConfigurationPage;
+
+class RosegardenConfigureDialog : public KDialogBase
 {
 Q_OBJECT
 public:
@@ -41,16 +123,17 @@ public:
 public slots:
     // Actions
     //
-    void slotOK();
-    void slotApply();
-    void slotClose();
+    virtual void slotOK();
+    virtual void slotApply();
+    virtual void slotClose();
 
     void slotActivateApply();
 
 private:
 
     RosegardenGUIDoc *m_doc;
-
+    GeneralConfigurationPage*  m_generalConfigurationPage;
+    PlaybackConfigurationPage* m_playbackConfigurationPage;
 };
 
 }
