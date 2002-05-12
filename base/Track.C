5abf05a3c05ecb0472113f9bae0eb0064dd678f2
cannam <cannam@cf6a83e7-f017-4305-b0b3-678bde24f230>
2002-05-12 18:36:02 +0000

    sorry, I hadn't intended to commit that debug output at all
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@2374 cf6a83e7-f017-4305-b0b3-678bde24f230

--- base/Track.C
+++ base/Track.C
@@ -71,13 +71,6 @@ std::string Track::toXmlString()
 
     std::stringstream track;
 
-    using std::cerr;
-    cerr << "Track::toXmlString: label is \"" << m_label << "\", bytes are:\n";
-    for (int i = 0; i < m_label.length(); ++i) {
-	cerr << (int)m_label[i] << " ";
-    }
-    cerr << std::endl;
-
     track << "<track id=\"" << m_id;
     track << "\" label=\"" << encode(m_label);
     track << "\" position=\"" << m_position;
