70933fb1b696e34cae6adf788c7a12c6159f8c4b
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2002-08-24 20:13:33 +0000

    Make ControlRuler repaint a bit more efficiently.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@2964 cf6a83e7-f017-4305-b0b3-678bde24f230

--- gui/controlruler.cpp
+++ gui/controlruler.cpp
@@ -115,10 +115,15 @@ ControlRuler::paintEvent(QPaintEvent* e)
     paint.setClipRegion(e->region());
     paint.setClipRect(e->rect().normalize());
 
-    //QRect clipRect = paint.clipRegion().boundingRect();
-    //paint.drawRect(0, 0, 100, 100);
+    QRect clipRect = paint.clipRegion().boundingRect();
 
-    Segment::iterator it = m_segment->begin();
+    timeT from = m_rulerScale->getTimeForX
+       (clipRect.x() - m_currentXOffset - 100 - m_xorigin);
+//     timeT   to = m_rulerScale->getTimeForX
+//        (clipRect.x() + clipRect.width() - m_currentXOffset + 100 - m_xorigin);
+
+
+    Segment::iterator it = m_segment->findNearestTime(from);
 
     for (; it != m_segment->end(); it++)
     {
@@ -126,6 +131,8 @@ ControlRuler::paintEvent(QPaintEvent* e)
         {
             int x = int(m_rulerScale->getXForTime((*it)->getAbsoluteTime()))
                     + m_currentXOffset + int(m_xorigin);
+
+            if (x > (clipRect.x() + clipRect.width())) break;
 
             int width = 
                 int(m_rulerScale->getXForTime((*it)->getAbsoluteTime() +
@@ -141,133 +148,6 @@ ControlRuler::paintEvent(QPaintEvent* e)
             paint.drawRect(x, height() - blockHeight, width, blockHeight);
         }
     }
-
-    /*
-    timeT from = m_rulerScale->getTimeForX
-       (clipRect.x() - m_currentXOffset - 100 - m_xorigin);
-    timeT   to = m_rulerScale->getTimeForX
-       (clipRect.x() + clipRect.width() - m_currentXOffset + 100 - m_xorigin);
-
-    QRect boundsForHeight = m_fontMetrics.boundingRect("^j|lM");
-    int fontHeight = boundsForHeight.height();
-    int textY = (height() - 6)/2 + fontHeight/2;
-
-    double prevEndX = -1000.0;
-    double prevTempo = 0.0;
-    long prevBpm = 0;
-
-    typedef std::map<timeT, bool> TimePoints;
-    int tempoChangeHere = 1, timeSigChangeHere = 2;
-    TimePoints timePoints;
-
-    for (int tempoNo = m_composition->getTempoChangeNumberAt(from) + 1;
-        tempoNo <= m_composition->getTempoChangeNumberAt(to); ++tempoNo) {
-
-       timePoints.insert
-           (TimePoints::value_type
-            (m_composition->getRawTempoChange(tempoNo).first,
-             tempoChangeHere));
-    }
-       
-    for (int sigNo = m_composition->getTimeSignatureNumberAt(from) + 1;
-        sigNo <= m_composition->getTimeSignatureNumberAt(to); ++sigNo) {
-
-       timeT time(m_composition->getTimeSignatureChange(sigNo).first);
-       if (timePoints.find(time) != timePoints.end()) {
-           timePoints[time] |= timeSigChangeHere;
-       } else {
-           timePoints.insert(TimePoints::value_type(time, timeSigChangeHere));
-       }
-    }
-
-    for (TimePoints::iterator i = timePoints.begin(); ; ++i) {
-
-       timeT t0, t1;
-
-       if (i == timePoints.begin()) {
-           t0 = from;
-       } else {
-           TimePoints::iterator j(i);
-           --j;
-           t0 = j->first;
-       }
-
-       if (i == timePoints.end()) {
-           t1 = to;
-       } else {
-           t1 = i->first;
-       }
-
-       QColor colour = TempoColour::getColour(m_composition->getTempoAt(t0));
-        paint.setPen(colour);
-        paint.setBrush(colour);
-
-       double x0, x1;
-       x0 = m_rulerScale->getXForTime(t0) + m_currentXOffset + m_xorigin;
-       x1 = m_rulerScale->getXForTime(t1) + m_currentXOffset + m_xorigin;
-        paint.drawRect(x0, 0, x1 - x0, height());
-
-       if (i == timePoints.end()) break;
-    }
-
-    paint.setPen(Qt::black);
-    paint.setBrush(Qt::black);
-
-    for (TimePoints::iterator i = timePoints.begin();
-        i != timePoints.end(); ++i) {
-
-       timeT time = i->first;
-       double x = m_rulerScale->getXForTime(time) + m_currentXOffset
-                   + m_xorigin;
-       
-       if (i->second & timeSigChangeHere) {
-
-           Rosegarden::TimeSignature sig =
-              m_composition->getTimeSignatureAt(time);
-
-           QString numStr = QString("%1").arg(sig.getNumerator());
-           QString denStr = QString("%1").arg(sig.getDenominator());
-
-           QRect numBounds = m_fontMetrics.boundingRect(numStr);
-           QRect denBounds = m_fontMetrics.boundingRect(denStr);
-
-           paint.setFont(m_boldFont);
-           paint.drawText(x - numBounds.width()/2,
-                        numBounds.height(), numStr);
-           paint.drawText(x - denBounds.width()/2,
-                        numBounds.height() + denBounds.height(), denStr);
-       }
-
-       if (i->second & tempoChangeHere) { 
-       
-           double tempo = m_composition->getTempoAt(time);
-           long bpm = long(tempo);
-
-           QString tempoString = QString("%1").arg(bpm);
-           if (tempo == prevTempo) {
-              if (m_small) continue;
-              tempoString = "=";
-           } else if (bpm == prevBpm) {
-              tempoString = (tempo > prevTempo ? "+" : "-");
-           } else {
-              if (m_small && (bpm != (bpm / 10 * 10))) {
-                  if (bpm == prevBpm + 1) tempoString = "+";
-                  else if (bpm == prevBpm - 1) tempoString = "-";
-              }
-           }
-           prevTempo = tempo;
-           prevBpm = bpm;
-
-           QRect bounds = m_fontMetrics.boundingRect(tempoString);
-
-           paint.setFont(m_font);
-           if (x > bounds.width() / 2) x -= bounds.width() / 2;
-           if (prevEndX >= x - 3) x = prevEndX + 3;
-           paint.drawText(x, textY, tempoString);
-           prevEndX = x + bounds.width();
-       }
-    }
-    */
 }
 
 ControlBox::ControlBox(QString label,
