282c5580fb961966b29b0df77658f8f981dc1e0b
bownie <bownie@cf6a83e7-f017-4305-b0b3-678bde24f230>
2002-11-10 16:43:57 +0000

    Some fiddling with threads.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@3311 cf6a83e7-f017-4305-b0b3-678bde24f230

--- base/test/thread.C
+++ base/test/thread.C
@@ -5,6 +5,8 @@
 
 #include "Lock.h"
 #include "Composition.h"
+#include "Segment.h"
+#include "Event.h"
 
 #include <cstdio>
 #include <sys/times.h>
@@ -20,6 +22,46 @@ static void*
 writer_thread1(void *arg)
 {
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+    cout << "write_thread1 - init" << endl;
+
+    Rosegarden::Composition *comp = 
+        static_cast<Rosegarden::Composition*>(arg);
+
+    Rosegarden::Composition::segmentcontainer segs = comp->getSegments();
+    Rosegarden::Composition::segmentcontainer::iterator it = segs.begin();
+    Rosegarden::Segment *segment = *it;
+
+    Rosegarden::timeT insertTime = 50000;
+    while (true)
+    {
+        usleep(90000);
+        cout << "LENGTH = " << comp->getNbBars() << endl;
+        segment->insert(new Event(Note::EventType, insertTime));
+        insertTime += 96;
+    }
+}
+
+static void*
+write_thread2(void *arg)
+{
+    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+    cout << "write_thread2 - init" << endl;
+
+    Rosegarden::Composition *comp = 
+        static_cast<Rosegarden::Composition*>(arg);
+
+    Rosegarden::Composition::segmentcontainer segs = comp->getSegments();
+    Rosegarden::Composition::segmentcontainer::iterator it = segs.begin();
+    Rosegarden::Segment *segment = *it;
+
+    Rosegarden::timeT insertTime = 0;
+    while (true)
+    {
+        usleep(50);
+        cout << "LENGTH = " << comp->getNbBars() << endl;
+        segment->insert(new Event(Note::EventType, insertTime));
+        insertTime += 96;
+    }
 }
 
 
@@ -34,8 +76,23 @@ main(int argc, char **argv)
     pthread_t thread1;
     pthread_t thread2;
     Rosegarden::Composition comp;
+    Rosegarden::Segment segment;
+    comp.addSegment(&segment);
 
-    pthread_create(&thread1, 0, writer_thread1, *argv);
+    if (pthread_create(&thread1, 0, writer_thread1, &comp))
+    {
+        cerr << "Couldn't start thread 1" << endl;
+        exit(1);
+    }
+    pthread_detach(thread1);
+
+    if (pthread_create(&thread2, 0, write_thread2, &comp))
+    {
+        cerr << "Couldn't start thread 2" << endl;
+        exit(1);
+    }
+    pthread_detach(thread2);
+
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
 
     static Lock lock;
@@ -54,12 +111,15 @@ main(int argc, char **argv)
         cout << "couldn't get second write lock" << endl;
     }
 
+    Rosegarden::timeT insertTime = 0;
     while(true)
     {
-        cout << "Sleeping" << endl;
         usleep(50000);
-    }
 
+        cout << "Inserting Event at time " << insertTime << endl;
+        segment.insert(new Event(Note::EventType, insertTime));
+        insertTime += 96;
+    }
 
 };
 
