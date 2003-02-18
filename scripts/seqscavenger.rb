753787c4a0f23a15852f36d8e662c06ca4d9f4e2
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2003-02-18 00:48:42 +0000

    core files cleanup more clever.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@3756 cf6a83e7-f017-4305-b0b3-678bde24f230

--- scripts/seqscavenger.rb
+++ scripts/seqscavenger.rb
@@ -29,12 +29,24 @@ def check_core_files
 
     rgMainExecMTime = File.stat("gui/rosegarden").mtime
 
-    coreFilesToDelete = Dir["gui/core.*"].find_all { |x| File.stat(x).mtime < rgMainExecMTime }
+    # Remove core files older than binary
+    obsoleteCoreFiles = (Dir["gui/core.*"].sort{ |a,b| File.stat(a).mtime <=> File.stat(b).mtime })
+
+    mostRecentCoreFile = obsoleteCoreFiles.pop
+
+    if (File.stat(mostRecentCoreFile).mtime <  rgMainExecMTime)
+      # it's obsolete too, so put it back in obsoleteCoreFiles
+      obsoleteCoreFiles << mostRecentCoreFile
+    end
+
+#     obsoleteCoreFiles = Dir["gui/core.*"].find_all { |x| File.stat(x).mtime < rgMainExecMTime }
     
-    coreFilesToDelete.each do |file|
+    obsoleteCoreFiles.each do |file|
       puts "Found obsolete core file to delete : #{file}"
       File.delete file
     end
+
+    # Remove all but most recent core file
 
   rescue Exception
     # No exec, no big deal. We don't care.
