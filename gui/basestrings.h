a79a89225669b7b4a0b75c92f2677ac20483451b
hansk <hansk@cf6a83e7-f017-4305-b0b3-678bde24f230>
2003-04-01 17:20:09 +0000

    Dummy file to hold otherwise untranslated strings
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@3999 cf6a83e7-f017-4305-b0b3-678bde24f230

---
+++ gui/basestrings.h
@@ -0,0 +1,52 @@
+// -*- c-basic-offset: 4 -*-
+
+/*
+    Rosegarden-4
+    A sequencer and musical notation editor.
+
+    This program is Copyright 2000-2002
+        Guillaume Laurent   <glaurent@telegraph-road.org>,
+        Chris Cannam        <cannam@all-day-breakfast.com>,
+        Richard Bown        <bownie@bownie.com>
+
+    The moral right of the authors to claim authorship of this work
+    has been asserted.
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version.  See the file
+    COPYING included with this distribution for more information.
+*/
+
+/**
+ * DO NOT INCLUDE THIS FILE ANYWHERE.  It is only used for generating
+ * rosegarden.pot.  The actual strings should be included from their "source"
+ * locations.
+ * 
+ * This file contains strings from base/ that need to be
+ * translated; eventually it should be deprecated in favor
+ * of a more robust solution.
+ * 
+ *  Why is this not robust?  As it currently stands, you must update 
+ *  strings in at least two places (here and base/) every time something
+ *  changes.  Getting it wrong only affects translation, though, which
+ *  is arguably not too awful.
+ */
+
+i18n("Add accent");
+i18n("Add tenuto");
+i18n("Add staccato");
+i18n("Add staccatissimo");
+i18n("Add marcato");
+i18n("Add trill");
+i18n("Add turn");
+i18n("Add pause");
+i18n("Add up-bow");
+i18n("Add down-bow");
+i18n("Show Notes Toolbar");
+i18n("Show Rests Toolbar");
+i18n("Show Accidentals Toolbar");
+i18n("Show Clefs Toolbar");
+i18n("Show Marks Toolbar");
+i18n("Show Group Toolbar");
