444764ebf35a28e3cf80beda4728e573bb652089
cannam <cannam@cf6a83e7-f017-4305-b0b3-678bde24f230>
2003-06-28 17:43:47 +0000

    ...
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@4406 cf6a83e7-f017-4305-b0b3-678bde24f230

---
+++ base/MidiProgram.C
@@ -0,0 +1,151 @@
+// -*- c-basic-offset: 4 -*-
+
+/*
+    Rosegarden-4
+    A sequencer and musical notation editor.
+
+    This program is Copyright 2000-2003
+        Guillaume Laurent   <glaurent@telegraph-road.org>,
+        Chris Cannam        <cannam@all-day-breakfast.com>,
+        Richard Bown        <bownie@bownie.com>
+
+    The moral right of the authors to claim authorship of this work
+    has been asserted.
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version.  See the file
+    COPYING included with this distribution for more information.
+*/
+
+#include "MidiProgram.h"
+
+namespace Rosegarden {
+
+MidiBank::MidiBank()  : 
+    m_percussion(false), m_msb(0), m_lsb(0), m_name()
+{
+    // nothing else
+}
+
+MidiBank::MidiBank(bool percussion, MidiByte msb, MidiByte lsb, std::string name) :
+    m_percussion(percussion), m_msb(msb), m_lsb(lsb), m_name(name)
+{
+    // nothing else
+}
+
+bool
+MidiBank::operator==(const MidiBank &b) const
+{
+    return m_percussion == b.m_percussion && m_msb == b.m_msb && m_lsb == b.m_lsb;
+}
+    
+bool
+MidiBank::isPercussion() const
+{
+    return m_percussion;
+}
+
+MidiByte
+MidiBank::getMSB() const
+{
+    return m_msb;
+}
+
+MidiByte
+MidiBank::getLSB() const
+{
+    return m_lsb;
+}
+
+std::string
+MidiBank::getName() const
+{
+    return m_name;
+}
+
+void
+MidiBank::setName(std::string name)
+{
+    m_name = name; 
+}
+
+
+MidiProgram::MidiProgram() :
+    m_bank(), m_program(0), m_name()
+{
+    // nothing else
+}
+
+MidiProgram::MidiProgram(const MidiBank &bank, MidiByte program, std::string name) :
+    m_bank(bank), m_program(program), m_name(name)
+{
+    // nothing else
+}
+
+bool
+MidiProgram::operator==(const MidiProgram &p) const
+{
+    return m_bank == p.m_bank && m_program == p.m_program;
+}
+    
+const MidiBank &
+MidiProgram::getBank() const
+{
+    return m_bank;
+}
+
+MidiByte
+MidiProgram::getProgram() const
+{
+    return m_program;
+}
+
+std::string
+MidiProgram::getName() const
+{
+    return m_name;
+}
+
+void
+MidiProgram::setName(std::string name)
+{
+    m_name = name;
+}
+
+
+MidiMetronome::MidiMetronome(const MidiProgram &program, MidiByte pitch,
+			     InstrumentId instrument) :
+    m_program(program), m_pitch(pitch), m_instrument(instrument)
+{
+    // nothing else
+}
+
+bool
+MidiMetronome::operator==(const MidiMetronome &m) const
+{
+    return m_program == m.m_program && m_pitch == m.m_pitch &&
+	m_instrument == m.m_instrument;
+}
+
+const MidiProgram &
+MidiMetronome::getProgram() const
+{
+    return m_program;
+}
+
+MidiByte
+MidiMetronome::getPitch() const
+{
+    return m_pitch;
+}
+
+InstrumentId
+MidiMetronome::getInstrument() const
+{
+    return m_instrument;
+}
+
+}
+
---
+++ base/MidiProgram.h
@@ -0,0 +1,99 @@
+// -*- c-basic-offset: 4 -*-
+
+/*
+    Rosegarden-4
+    A sequencer and musical notation editor.
+
+    This program is Copyright 2000-2003
+        Guillaume Laurent   <glaurent@telegraph-road.org>,
+        Chris Cannam        <cannam@all-day-breakfast.com>,
+        Richard Bown        <bownie@bownie.com>
+
+    The moral right of the authors to claim authorship of this work
+    has been asserted.
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version.  See the file
+    COPYING included with this distribution for more information.
+*/
+
+#ifndef _MIDIBANK_H_
+#define _MIDIBANK_H_
+
+#include <string>
+
+namespace Rosegarden
+{
+typedef unsigned char MidiByte;
+typedef unsigned int InstrumentId;
+
+class MidiBank
+{
+public:
+    MidiBank();
+    MidiBank(bool percussion, MidiByte msb, MidiByte lsb, std::string name = "");
+
+    // comparator disregards name
+    bool operator==(const MidiBank &b) const;
+    
+    bool                isPercussion() const;
+    MidiByte            getMSB() const;
+    MidiByte            getLSB() const;
+    std::string         getName() const;
+
+    void                setName(std::string name);
+
+private:
+    bool m_percussion;
+    MidiByte m_msb;
+    MidiByte m_lsb;
+    std::string m_name;
+};
+
+class MidiProgram
+{
+public:
+    MidiProgram();
+    MidiProgram(const MidiBank &bank, MidiByte program, std::string name = "");
+
+    // comparator disregards name
+    bool operator==(const MidiProgram &p) const;
+    
+    const MidiBank&     getBank() const;
+    MidiByte            getProgram() const;
+    std::string         getName() const;
+
+    void                setName(std::string name);
+
+private:
+    MidiBank m_bank;
+    MidiByte m_program;
+    std::string m_name;
+};
+
+// A mapped MIDI instrument - a drum track click for example
+//
+class MidiMetronome
+{
+public:
+    MidiMetronome(const MidiProgram &program, MidiByte pitch,
+		  InstrumentId instrument);
+
+    bool operator==(const MidiMetronome &m) const;
+
+    const MidiProgram&  getProgram() const;
+    MidiByte            getPitch() const;
+    InstrumentId        getInstrument() const;
+
+private:
+    MidiProgram m_program;
+    MidiByte m_pitch;
+    InstrumentId m_instrument;
+};
+
+}
+
+#endif
+
