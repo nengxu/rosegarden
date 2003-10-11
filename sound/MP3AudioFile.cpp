7b369cce83784eb5b2b4314863c85ed0ed6e7fe2
bownie <bownie@cf6a83e7-f017-4305-b0b3-678bde24f230>
2003-10-11 14:04:46 +0000

    Tiny changes.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@4667 cf6a83e7-f017-4305-b0b3-678bde24f230

--- sound/MP3AudioFile.cpp
+++ sound/MP3AudioFile.cpp
@@ -24,6 +24,8 @@
 
 #ifdef HAVE_LIBMAD
 
+#include <mad.h>
+
 namespace Rosegarden
 {
 
@@ -37,11 +39,11 @@ MP3AudioFile::MP3AudioFile(const unsigned int &id,
 
 
 MP3AudioFile::MP3AudioFile(const std::string &fileName,
-                           unsigned int channels,
-                           unsigned int sampleRate,
-                           unsigned int bytesPerSecond,
-                           unsigned int bytesPerSample,
-                           unsigned int bitsPerSample):
+                           unsigned int /*channels*/,
+                           unsigned int /*sampleRate*/,
+                           unsigned int /*bytesPerSecond*/,
+                           unsigned int /*bytesPerSample*/,
+                           unsigned int /*bitsPerSample*/):
     AudioFile(0, std::string(""), fileName)
 {
     m_type = MP3;
@@ -77,11 +79,7 @@ MP3AudioFile::open()
     }
     catch(std::string s)
     {
-        //throw(s);
-#ifdef DEBUG_MP3
-        std::cout << "MP3AudioFile::open() parseHeader threw " << s << endl;
-#endif
-        return false;
+        throw(s);
     }
 
     return true;
@@ -90,6 +88,7 @@ MP3AudioFile::open()
 bool
 MP3AudioFile::write()
 {
+    return false;
 }
 
 void
@@ -100,6 +99,52 @@ MP3AudioFile::close()
 void
 MP3AudioFile::parseHeader()
 {
+    const std::string MP3_TAG("TAG");
+    if (m_inFile == 0)
+        return;
+
+    // seek to beginning
+    m_inFile->seekg(0, std::ios::beg);
+
+    // get the header string
+    //
+    std::string hS = getBytes(256);
+    int headerLength = 0;
+
+    if (hS.substr(0, 3) == MP3_TAG) 
+        headerLength = 127;
+    else if (hS.substr(0, 4) == std::string("RIFF"))
+        headerLength = 20; // guess for the moment
+    else
+    {
+        headerLength = 10;
+    }
+
+    mad_synth synth;
+    mad_frame frame;
+    mad_stream stream;
+
+    mad_synth_init(&synth);
+    mad_stream_init(&stream);
+    mad_frame_init(&frame);
+
+    /*
+    mad_stream_buffer(&stream, hS.data(), hS.length());
+
+    if (mad_header_decode(&frame.header, &stream) == -1)
+    {
+        throw("Can't decode header");
+    }
+
+    mad_frame_decode(&frame, &stream);
+
+    m_sampleRate = frame.header.samplerate;
+
+    mad_synth_frame(&synth, &frame);
+    struct mad_pcm *pcm = &synth.pcm;
+
+    m_channels = pcm->channels;
+    */
 }
 
 std::streampos
@@ -109,13 +154,13 @@ MP3AudioFile::getDataOffset()
 }
 
 bool 
-MP3AudioFile::scanTo(const RealTime &time)
+MP3AudioFile::scanTo(const RealTime &/*time*/)
 {
     return false;
 }
 
 bool
-MP3AudioFile::scanTo(std::ifstream *file, const RealTime &time)
+MP3AudioFile::scanTo(std::ifstream * /*file*/, const RealTime &/*time*/)
 {
     return false;
 }
@@ -124,13 +169,13 @@ MP3AudioFile::scanTo(std::ifstream *file, const RealTime &time)
 // Scan forward in a file by a certain amount of time
 //
 bool 
-MP3AudioFile::scanForward(const RealTime &time)
+MP3AudioFile::scanForward(const RealTime &/*time*/)
 {
     return false;
 }
 
 bool
-MP3AudioFile::scanForward(std::ifstream *file, const RealTime &time)
+MP3AudioFile::scanForward(std::ifstream * /*file*/, const RealTime &/*time*/)
 {
     return false;
 }
@@ -140,14 +185,14 @@ MP3AudioFile::scanForward(std::ifstream *file, const RealTime &time)
 // de-interleave n-channel samples themselves.
 //
 std::string
-MP3AudioFile::getSampleFrames(std::ifstream *file,
-                              unsigned int frames)
+MP3AudioFile::getSampleFrames(std::ifstream * /*file*/,
+                              unsigned int /*frames*/)
 {
     return "";
 }
 
 std::string
-MP3AudioFile::getSampleFrames(unsigned int frames)
+MP3AudioFile::getSampleFrames(unsigned int /*frames*/)
 {
     return "";
 }
@@ -157,14 +202,14 @@ MP3AudioFile::getSampleFrames(unsigned int frames)
 // over a time slice from current file pointer position.
 //
 std::string 
-MP3AudioFile::getSampleFrameSlice(std::ifstream *file,
-                                  const RealTime &time)
+MP3AudioFile::getSampleFrameSlice(std::ifstream * /*file*/,
+                                  const RealTime &/*time*/)
 {
     return "";
 }
 
 std::string
-MP3AudioFile::getSampleFrameSlice(const RealTime &time)
+MP3AudioFile::getSampleFrameSlice(const RealTime & /*time*/)
 {
     return "";
 }
@@ -174,7 +219,7 @@ MP3AudioFile::getSampleFrameSlice(const RealTime &time)
 // audio file.
 //
 bool
-MP3AudioFile::appendSamples(const std::string &buffer)
+MP3AudioFile::appendSamples(const std::string &/*buffer*/)
 {
     return false;
 }
