7e9325937d7e7c16071bab178778b11e70dfe413
cannam <cannam@cf6a83e7-f017-4305-b0b3-678bde24f230>
2004-05-12 16:17:27 +0000

    * added test program for comparing queue timer against jack frame count
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@5526 cf6a83e7-f017-4305-b0b3-678bde24f230

--- base/test/seq/queue-timer-jack.c
+++ base/test/seq/queue-timer-jack.c
@@ -1,7 +1,10 @@
 
 #include <alsa/asoundlib.h>
 #include <alsa/seq.h>
+#include <jack/jack.h>
 #include <sys/time.h>
+
+static jack_nframes_t sample_frames = 0;
 
 void normalize(struct timeval *tv)
 {
@@ -18,6 +21,23 @@ void normalize(struct timeval *tv)
 }
 
 int
+jack_process(jack_nframes_t nframes, void *arg)
+{
+    sample_frames += nframes;
+}
+
+jack_nframes_t
+rt_to_frame(struct timeval tv, jack_nframes_t sample_rate)
+{
+    if (tv.tv_sec < 0) tv.tv_sec = -tv.tv_sec;
+    if (tv.tv_usec < 0) tv.tv_usec = -tv.tv_usec;
+    return
+	tv.tv_sec * sample_rate +
+	((tv.tv_usec / 1000) * sample_rate) / 1000 +
+	((tv.tv_usec - 1000 * (tv.tv_usec / 1000)) * sample_rate) / 1000000;
+}
+
+int
 main(int argc, char **argv)
 {
     snd_seq_t *handle;
@@ -27,10 +47,21 @@ main(int argc, char **argv)
     int queue;
     int i;
     int rval;
-    struct timeval starttv, prevdiff;
+    struct timeval starttv;
     int countdown = -1;
     snd_seq_queue_timer_t *timer;
     snd_timer_id_t *timerid;
+    jack_client_t *jclient;
+    jack_nframes_t sample_rate;
+    
+    if ((jclient = jack_client_new("queue-timer-jack")) == 0) {
+	fprintf(stderr, "failed to connect to JACK server\n");
+	return 1;
+    }
+
+    jack_set_process_callback(jclient, jack_process, 0);
+
+    sample_rate = jack_get_sample_rate(jclient);
 
     if (snd_seq_open(&handle, "hw", SND_SEQ_OPEN_DUPLEX, 0) < 0) {
 	fprintf(stderr, "failed to open ALSA sequencer interface\n");
@@ -50,7 +81,7 @@ main(int argc, char **argv)
 	fprintf(stderr, "failed to create ALSA sequencer queue\n");
 	return 1;
     }
-/*
+
     snd_seq_queue_timer_alloca(&timer);
     snd_seq_get_queue_timer(handle, queue, timer);
     snd_timer_id_alloca(&timerid);
@@ -61,19 +92,23 @@ main(int argc, char **argv)
     snd_timer_id_set_subdevice(timerid, 0);
     snd_seq_queue_timer_set_id(timer, timerid);
     snd_seq_set_queue_timer(handle, queue, timer);
-*/
+
+    if (jack_activate(jclient)) {
+        fprintf (stderr, "cannot activate jack client");
+        exit(1);
+    }
+
     snd_seq_start_queue(handle, queue, 0);
     snd_seq_drain_output(handle);
 
     gettimeofday(&starttv, 0);
-    prevdiff.tv_sec = 0;
-    prevdiff.tv_usec = 0;
 
     while (countdown != 0) {
 
 	snd_seq_queue_status_t *status;
 	const snd_seq_real_time_t *rtime;
-	struct timeval tv, diff, diffdiff;
+	struct timeval tv, qtv, jtv, diff, jdiff;
+	jack_nframes_t frames_now;
 
 	snd_seq_queue_status_alloca(&status);
 
@@ -82,36 +117,34 @@ main(int argc, char **argv)
 
 	gettimeofday(&tv, 0);
 
+	frames_now = sample_frames;
+	fprintf(stderr, "    frames: %ld\n", frames_now);
+
+	qtv.tv_sec = rtime->tv_sec;
+	qtv.tv_usec = rtime->tv_nsec / 1000;
+
 	tv.tv_sec -= starttv.tv_sec;
 	tv.tv_usec -= starttv.tv_usec;
 	normalize(&tv);
 
-	diff.tv_sec = tv.tv_sec - rtime->tv_sec;
-	diff.tv_usec = tv.tv_usec - rtime->tv_nsec / 1000;
+	jtv.tv_sec = frames_now / sample_rate;
+	frames_now -= jtv.tv_sec * sample_rate;
+	jtv.tv_usec = (int)(((float)frames_now * 1000000) / sample_rate);
+
+	diff.tv_sec = tv.tv_sec - qtv.tv_sec;
+	diff.tv_usec = tv.tv_usec - qtv.tv_usec;
 	normalize(&diff);
 
-	diffdiff.tv_sec = diff.tv_sec - prevdiff.tv_sec;
-	diffdiff.tv_usec = diff.tv_usec - prevdiff.tv_usec;
-	normalize(&diffdiff);
-	prevdiff = diff;
+	jdiff.tv_sec = jtv.tv_sec - qtv.tv_sec;
+	jdiff.tv_usec = jtv.tv_usec - qtv.tv_usec;
+	normalize(&jdiff);
 
-	fprintf(stderr, " real time: %12ld sec %8ld usec\nqueue time: %12ld sec %8ld usec\n      diff: %12ld sec %8ld usec\n  diffdiff: %12ld sec %8ld usec\n",
-		tv.tv_sec, tv.tv_usec,
-		rtime->tv_sec, rtime->tv_nsec / 1000,
-		diff.tv_sec, diff.tv_usec,
-		diffdiff.tv_sec, diffdiff.tv_usec);
-
-	if (diffdiff.tv_usec >  5000 ||
-	    diffdiff.tv_usec < -5000) {
-	    fprintf(stderr, "oops! queue slipped\n");
-	    if (tv.tv_sec < 5) {
-		fprintf(stderr, "(ignoring in first few seconds)\n");
-	    } else {
-		countdown = 2;
-	    }
-	} else {
-	    if (countdown > 0) --countdown;
-	}
+	fprintf(stderr, " real time: %12ld sec %8ld usec /%12ld frames\nqueue time: %12ld sec %8ld usec /%12ld frames\n jack time: %12ld sec %8ld usec /%12ld frames\n   rq diff: %12ld sec %8ld usec /%12ld frames\n   jq diff: %12ld sec %8ld usec /%12ld frames\n",
+		tv.tv_sec, tv.tv_usec, rt_to_frame(tv, sample_rate),
+		qtv.tv_sec, qtv.tv_usec, rt_to_frame(qtv, sample_rate),
+		jtv.tv_sec, jtv.tv_usec, rt_to_frame(jtv, sample_rate),
+		diff.tv_sec, diff.tv_usec, rt_to_frame(diff, sample_rate),
+		jdiff.tv_sec, jdiff.tv_usec, rt_to_frame(jdiff, sample_rate));
 
 	fprintf(stderr, "\n");
 	struct timespec ts;
