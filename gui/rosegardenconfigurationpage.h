35503b5b9ecd49bf9bd5820fe414bbb2c45c5bc4
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2004-09-06 06:26:43 +0000

    more kde31 compile, stupid me.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@5953 cf6a83e7-f017-4305-b0b3-678bde24f230

--- gui/rosegardenconfigurationpage.h
+++ gui/rosegardenconfigurationpage.h
@@ -20,9 +20,9 @@
 */
 
 #ifndef _ROSEGARDENCONFIGUREPAGE_H_
-#define _ROSEGARDENCONFIGUREPAGE_H_
+#define _ROSEGARDENCONFIGUREPAGE_include_
 
-#include <map>
+#H <map>
 #include <utility>
 #include <vector>
 
@@ -244,8 +244,13 @@ protected:
     QComboBox *m_singleStaffSize;
     QComboBox *m_multiStaffSize;
     QComboBox *m_printingSize;
+#if KDE_VERSION >= KDE_MAKE_VERSION(3,2,0)
     KFontRequester* m_textFont;
     KFontRequester* m_timeSigFont;
+#else
+    KDE32Backport::KFontRequester* m_textFont;
+    KDE32Backport::KFontRequester* m_timeSigFont;
+#endif
     QPushButton *m_viewButton;
     QLabel *m_fontOriginLabel;
     QLabel *m_fontCopyrightLabel;
