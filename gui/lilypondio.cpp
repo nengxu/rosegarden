ae7e835fdd5b5eb7bf81e3a6c3be1e531181de6a
dmmcintyr <dmmcintyr@cf6a83e7-f017-4305-b0b3-678bde24f230>
2005-06-12 16:30:55 +0000

    back out bogus bug fix
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@6602 cf6a83e7-f017-4305-b0b3-678bde24f230

--- gui/lilypondio.cpp
+++ gui/lilypondio.cpp
@@ -730,11 +730,9 @@ LilypondExporter::write()
 
         // do nothing if track is muted...  this provides a crude but easily implemented
         // method for users to selectively export tracks...
-//        Rosegarden::Track *track = m_composition->getTrackById((*i)->getTrack());
-        // fix bug #1218737 Lillypond export should follow track order
-        Rosegarden::Track *track = m_composition->getTrackByPosition(trackNo);
+        Rosegarden::Track *track = m_composition->getTrackById((*i)->getTrack());
         
-        if (!exportUnmuted || (!track->isMuted())) {
+	if (!exportUnmuted || (!track->isMuted())) {
             if ((int) (*i)->getTrack() != lastTrackIndex) {
                 if (lastTrackIndex != -1) {
                     // close the old track (Staff context)
