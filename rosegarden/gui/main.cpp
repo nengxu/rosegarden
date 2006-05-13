8fb6357ee8857acdc5377a4c6cbd8195b7ce2173
glaurent <glaurent@cf6a83e7-f017-4305-b0b3-678bde24f230>
2006-05-13 14:39:52 +0000

    test the 'k_funcinfo' debug operator - ignore
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@7260 cf6a83e7-f017-4305-b0b3-678bde24f230

--- rosegarden/gui/main.cpp
+++ rosegarden/gui/main.cpp
@@ -506,7 +506,7 @@ int main(int argc, char *argv[])
     //
     if (config->readBoolEntry("Logo",true) && (!kapp->isRestored() && args->isSet("splash")) )
     {
-        RG_DEBUG << "main: Showing startup logo\n";
+        RG_DEBUG << k_funcinfo << "Showing startup logo\n";
         startLogo = KStartupLogo::getInstance();
         startLogo->show();
     }
