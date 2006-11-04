e0ae0e62547a076f2139750251c3d69ba1166cd7
plcl <plcl@cf6a83e7-f017-4305-b0b3-678bde24f230>
2006-11-04 18:47:19 +0000

    PitchPickerDialog moved from widgets to dialogs
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/branches/reorganisation@7622 cf6a83e7-f017-4305-b0b3-678bde24f230

--- rosegarden/src/gui/dialogs/PitchPickerDialog.cpp
+++ rosegarden/src/gui/dialogs/PitchPickerDialog.cpp
@@ -1,21 +1,20 @@
-
 /* -*- c-basic-offset: 4 indent-tabs-mode: nil -*- vi:set ts=8 sts=4 sw=4: */
 
 /*
     Rosegarden
     A MIDI and audio sequencer and musical notation editor.
-
+ 
     This program is Copyright 2000-2006
         Guillaume Laurent   <glaurent@telegraph-road.org>,
         Chris Cannam        <cannam@all-day-breakfast.com>,
         Richard Bown        <richard.bown@ferventsoftware.com>
-
+ 
     The moral rights of Guillaume Laurent, Chris Cannam, and Richard
     Bown to claim authorship of this work have been asserted.
-
+ 
     Other copyrights also apply to some parts of this work.  Please
     see the AUTHORS file and individual file headers for details.
-
+ 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License as
     published by the Free Software Foundation; either version 2 of the
@@ -23,35 +22,39 @@
     COPYING included with this distribution for more information.
 */
 
-#ifndef _RG_PITCHPICKERDIALOG_H_
-#define _RG_PITCHPICKERDIALOG_H_
 
-#include "PitchChooser.h"
+#include "PitchPickerDialog.h"
+#include <klocale.h>
 #include <kdialogbase.h>
-
-
-class QWidget;
+#include <qlayout.h>
+#include <qframe.h>
+#include <qstring.h>
+#include <qvbox.h>
+#include <qwidget.h>
 
 
 namespace Rosegarden
 {
 
-class PitchPickerDialog : public KDialogBase
+PitchPickerDialog::PitchPickerDialog(QWidget *parent, int initialPitch, bool isFrom) :
+        KDialogBase(parent, 0, true, i18n("Pitch Selector"), Ok | Cancel)
 {
-    Q_OBJECT
+    QVBox *vBox = makeVBoxMainWidget();
 
-public:
+    QFrame *frame = new QFrame(vBox);
 
-    PitchPickerDialog(QWidget* parent, int initialPitch, bool isFrom);
-    ~PitchPickerDialog();
+    QGridLayout *layout = new QGridLayout(frame, 4, 3, 10, 5);
 
-    int getPitch() { return m_pitch->getPitch(); }
-    
-private:
-    PitchChooser* m_pitch;
-};
+    QString toFrom = (isFrom ? "Starting" : "Ending");
 
-
+    m_pitch = new PitchChooser(i18n("%1 Pitch").arg(toFrom), frame, initialPitch);
+    layout->addMultiCellWidget(m_pitch, 0, 0, 0, 2, Qt::AlignHCenter);
 }
 
-#endif
+PitchPickerDialog::~PitchPickerDialog()
+{
+    // Nothing here...
+}
+
+}
+#include "PitchPickerDialog.moc"
