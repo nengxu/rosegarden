c11b2c961886e1cd53b0d9a6c9fe829a1a527df3
raboofje <raboofje@cf6a83e7-f017-4305-b0b3-678bde24f230>
2007-04-22 14:05:38 +0000

    some simple unittest-ish tests for accidental-resolving
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@8020 cf6a83e7-f017-4305-b0b3-678bde24f230

--- src/base/test/transpose.cpp
+++ src/base/test/transpose.cpp
@@ -9,6 +9,21 @@ using std::cout;
 // 
 // Returns -1 (or crashes :)) on error, 0 on success
 
+/**
+ * should be in Pitch eventually
+ */
+void testAisDisplayAccidentalInCmaj()
+{
+    Pitch ais(70, Accidentals::Sharp);
+    Key cmaj ("C major");
+    Accidental accidental = ais.getDisplayAccidental(cmaj);
+    if (accidental != Accidentals::Sharp)
+    {
+        std::cout << "Accidental for A# in Cmaj was " << accidental << " instead of expected Sharp" << std::endl;
+        exit(-1);
+    }
+}
+
 /** 
  * transpose an A# up by a major second, should 
  * yield a B# (as C would be a minor triad) 
@@ -17,15 +32,15 @@ void testAisToBis()
 {
     std::cout << "Testing transposing A# to B#... ";
     Pitch ais(70, Accidentals::Sharp);
-    Key* cmaj = new Key();
+    Key cmaj ("C major");
 
-    Pitch result = ais.transpose(*cmaj, 2, 1);
+    Pitch result = ais.transpose(cmaj, 2, 1);
 
-    Accidental resultAccidental = result.getAccidental(*cmaj);
+    Accidental resultAccidental = result.getAccidental(cmaj);
     int resultPitch = result.getPerformancePitch();
     if (resultAccidental != Accidentals::Sharp || resultPitch != 72)
     {
-        std::cout << "Transposing A# up by a major second didn't yield B#, but " << result.getNoteName(*cmaj) << resultAccidental << std::endl;
+        std::cout << "Transposing A# up by a major second didn't yield B#, but " << result.getNoteName(cmaj) << resultAccidental << std::endl;
         exit(-1);
     }
     std::cout << "Success" << std::endl;
@@ -44,7 +59,7 @@ void testGToD()
 
     Accidental resultAccidental = result.getAccidental(*dmaj);
     int resultPitch = result.getPerformancePitch();
-    if (resultAccidental != Accidentals::Natural || resultPitch != 74)
+    if (resultAccidental != Accidentals::NoAccidental || resultPitch != 74)
     {
         std::cout << "Transposing G up by a fifth didn't yield D, but " << result.getNoteName(*dmaj) << resultAccidental << std::endl;
         exit(-1);
@@ -59,6 +74,7 @@ void testKeyTransposition()
 
 int main(int argc, char **argv)
 {
+    testAisDisplayAccidentalInCmaj();
     testAisToBis();
     testGToD();
     testKeyTransposition();
