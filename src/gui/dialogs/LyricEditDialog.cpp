b3fc0c17d6a193722a21efcafbba70f006d9f95a
plcl <plcl@cf6a83e7-f017-4305-b0b3-678bde24f230>
2007-09-24 22:03:38 +0000

    Fix a i18n problem
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@8239 cf6a83e7-f017-4305-b0b3-678bde24f230

--- src/gui/dialogs/LyricEditDialog.cpp
+++ src/gui/dialogs/LyricEditDialog.cpp
@@ -81,7 +81,7 @@ LyricEditDialog::LyricEditDialog(QWidget *parent,
     unparse();
     
     for (int i = 0; i < m_verseCount; ++i) {
-        m_verseNumber->insertItem(QString("Verse %1").arg(i + 1));
+        m_verseNumber->insertItem(i18n("Verse %1").arg(i + 1));
     }
     m_currentVerse = 0;
     if (m_verseCount == 12) m_verseAddButton->setEnabled(false);
@@ -103,7 +103,7 @@ LyricEditDialog::slotAddVerse()
     NOTATION_DEBUG << "LyricEditDialog::slotAddVerse" << endl;
     m_verseCount++;
     m_texts.push_back(m_skeleton);
-    m_verseNumber->insertItem(QString("Verse %1").arg(m_verseCount));
+    m_verseNumber->insertItem(i18n("Verse %1").arg(m_verseCount));
     m_verseNumber->setCurrentItem(m_verseCount - 1);
     slotVerseNumberChanged(m_verseCount - 1);
     if (m_verseCount == 12) m_verseAddButton->setEnabled(false);
