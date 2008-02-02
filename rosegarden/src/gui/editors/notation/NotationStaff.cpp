30fb0e84e9a577ceea1e411d8011e13622b1bb47
dmmcintyr <dmmcintyr@cf6a83e7-f017-4305-b0b3-678bde24f230>
2008-02-02 18:49:09 +0000

    nomenclature change
    Let's try this on for size.  I'm not that thrilled with "Convert notation
    for..." or whatever I came up with, but "switch instrument" just won't do at
    all, and neither should this be a 25-word essay explaining what the hell it's
    for, so let's see if this will work.
    
    
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@8602 cf6a83e7-f017-4305-b0b3-678bde24f230

--- rosegarden/src/gui/editors/notation/NotationStaff.cpp
+++ rosegarden/src/gui/editors/notation/NotationStaff.cpp
@@ -678,6 +678,7 @@ NotationStaff::positionElements(timeT from, timeT to)
                 if (!spanning) {
                     (void)(el->event()->get
                            <Bool>(BaseProperties::TIED_FORWARD, spanning));
+		    NOTATION_DEBUG << "DMM: Staff tied forward == " << (spanning ? "true" : "false") << endl;
                 }
 
                 if (spanning) {
