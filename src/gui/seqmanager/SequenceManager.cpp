b3a1abd204f90564f6773c340b1bbf3a70c53b24
cannam <cannam@cf6a83e7-f017-4305-b0b3-678bde24f230>
2008-03-18 11:30:12 +0000

    * merge fix from stable_1_6
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@8747 cf6a83e7-f017-4305-b0b3-678bde24f230

--- src/gui/seqmanager/SequenceManager.cpp
+++ src/gui/seqmanager/SequenceManager.cpp
@@ -1146,13 +1146,13 @@ SequenceManager::processAsynchronousMidi(const MappedComposition &mC,
                     if ((*i)->getData1() == MappedEvent::FailureJackRestartFailed) {
 
                         KMessageBox::error(
-                            dynamic_cast<QWidget*>(m_doc->parent())->parentWidget(),
+                            dynamic_cast<QWidget*>(m_doc->parent()),
                             i18n("The JACK Audio subsystem has failed or it has stopped Rosegarden from processing audio.\nPlease restart Rosegarden to continue working with audio.\nQuitting other running applications may improve Rosegarden's performance."));
 
                     } else if ((*i)->getData1() == MappedEvent::FailureJackRestart) {
 
                         KMessageBox::error(
-                            dynamic_cast<QWidget*>(m_doc->parent())->parentWidget(),
+                            dynamic_cast<QWidget*>(m_doc->parent()),
                             i18n("The JACK Audio subsystem has stopped Rosegarden from processing audio, probably because of a processing overload.\nAn attempt to restart the audio service has been made, but some problems may remain.\nQuitting other running applications may improve Rosegarden's performance."));
 
                     } else if ((*i)->getData1() == MappedEvent::WarningImpreciseTimer &&
@@ -1166,7 +1166,7 @@ SequenceManager::processAsynchronousMidi(const MappedComposition &mC,
                         RosegardenGUIApp::self()->awaitDialogClearance();
 
                         KMessageBox::information(
-                            dynamic_cast<QWidget*>(m_doc->parent())->parentWidget(),
+                            dynamic_cast<QWidget*>(m_doc->parent()),
                             i18n("<h3>System timer resolution is too low</h3><p>Rosegarden was unable to find a high-resolution timing source for MIDI performance.</p><p>This may mean you are using a Linux system with the kernel timer resolution set too low.  Please contact your Linux distributor for more information.</p><p>Some Linux distributors already provide low latency kernels, see <a href=\"http://rosegarden.wiki.sourceforge.net/Low+latency+kernels\">http://rosegarden.wiki.sourceforge.net/Low+latency+kernels</a> for instructions.</p>"), 
 			    NULL, NULL, 
 			    KMessageBox::Notify + KMessageBox::AllowLink);
@@ -1184,7 +1184,7 @@ SequenceManager::processAsynchronousMidi(const MappedComposition &mC,
                         RosegardenGUIApp::self()->awaitDialogClearance();
 
                         KMessageBox::information(
-                            dynamic_cast<QWidget*>(m_doc->parent())->parentWidget(),
+                            dynamic_cast<QWidget*>(m_doc->parent()),
                             i18n("<h3>System timer resolution is too low</h3><p>Rosegarden was unable to find a high-resolution timing source for MIDI performance.</p><p>You may be able to solve this problem by loading the RTC timer kernel module.  To do this, try running <b>sudo modprobe snd-rtctimer</b> in a terminal window and then restarting Rosegarden.</p><p>Alternatively, check whether your Linux distributor provides a multimedia-optimized kernel.  See <a href=\"http://rosegarden.wiki.sourceforge.net/Low+latency+kernels\">http://rosegarden.wiki.sourceforge.net/Low+latency+kernels</a> for notes about this.</p>"), 
 			    NULL, NULL, 
 			    KMessageBox::Notify + KMessageBox::AllowLink);
