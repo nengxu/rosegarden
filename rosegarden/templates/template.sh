b11561ca56842ccb645c81bc265b93ecbc5ec886
plcl <plcl@cf6a83e7-f017-4305-b0b3-678bde24f230>
2008-04-19 12:11:26 +0000

    Added file headers for build system scripts
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk@8830 cf6a83e7-f017-4305-b0b3-678bde24f230

---
+++ rosegarden/templates/template.sh
@@ -0,0 +1,19 @@
+# Rosegarden
+# A MIDI and audio sequencer and musical notation editor.
+#
+# This program is Copyright 2000-2008
+#     Guillaume Laurent   <glaurent@telegraph-road.org>,
+#     Chris Cannam        <cannam@all-day-breakfast.com>,
+#     Richard Bown        <richard.bown@ferventsoftware.com>
+#
+# The moral rights of Guillaume Laurent, Chris Cannam, and Richard
+# Bown to claim authorship of this work have been asserted.
+#
+# Other copyrights also apply to some parts of this work.  Please
+# see the AUTHORS file and individual file headers for details.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.  See the file
+# COPYING included with this distribution for more information.
