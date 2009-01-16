dfea059c8d9bfcc9ca1320b7721be17ae12b7703
dmmcintyr <dmmcintyr@cf6a83e7-f017-4305-b0b3-678bde24f230>
2009-01-16 19:44:43 +0000

    update templates w/ new copyright date, and to reflect preferred ordering of includes in new files
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/branches/qt4@9731 cf6a83e7-f017-4305-b0b3-678bde24f230

--- templates/template.h
+++ templates/template.h
@@ -3,7 +3,7 @@
 /*
     Rosegarden
     A MIDI and audio sequencer and musical notation editor.
-    Copyright 2000-2008 the Rosegarden development team.
+    Copyright 2000-2009 the Rosegarden development team.
 
     Other copyrights also apply to some parts of this work.  Please
     see the AUTHORS file and individual file headers for details.
@@ -17,6 +17,15 @@
 
 #ifndef _TEMPLATE_H_
 #define _TEMPLATE_H_
+
+// local includes
+#include <local/LocalInclude.h>
+
+// Qt includes
+#include <QtInclude>
+
+// STL & other includes
+#include <vector>
 
 namespace Rosegarden
 {
