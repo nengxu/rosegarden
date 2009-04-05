a365ef551c23a049fee415cb080579459b192606
dmmcintyr <dmmcintyr@cf6a83e7-f017-4305-b0b3-678bde24f230>
2009-04-05 02:42:29 +0000

    * these have done their job, and are no longer needed
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@10158 cf6a83e7-f017-4305-b0b3-678bde24f230

--- run-converter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-for x in `find src \( -name \*.h -o -name \*.cpp \)`; do
-    echo $x
-    if ! perl ./convert.pl "$x" > "$x".$$ ; then exit ; fi
-    cmp -s "$x".$$ "$x" || mv "$x".$$ "$x" && rm -f "$x".$$
-done
