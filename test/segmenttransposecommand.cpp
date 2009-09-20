2948169d77ca58973cea3b9622dd8bbd7c704e5f
dmmcintyr <dmmcintyr@cf6a83e7-f017-4305-b0b3-678bde24f230>
2009-09-20 18:51:22 +0000

    use the same emacs/vim line at the top of every file consistently
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@10915 cf6a83e7-f017-4305-b0b3-678bde24f230

--- test/segmenttransposecommand.cpp
+++ test/segmenttransposecommand.cpp
@@ -1,4 +1,4 @@
-// -*- c-basic-offset: 4 -*-
+/* -*- c-basic-offset: 4 indent-tabs-mode: nil -*- vi:set ts=8 sts=4 sw=4: */
 
 #include "commands/segment/SegmentTransposeCommand.h"
 #include "base/NotationTypes.h"
