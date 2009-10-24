8b5b70cd0d48b538b08b73d403eee01d174843b3
dmmcintyr <dmmcintyr@cf6a83e7-f017-4305-b0b3-678bde24f230>
2009-10-24 03:12:16 +0000

    fix Segments-Relabel
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@11092 cf6a83e7-f017-4305-b0b3-678bde24f230

--- src/gui/application/RosegardenMainWindow.cpp
+++ src/gui/application/RosegardenMainWindow.cpp
@@ -6243,7 +6243,7 @@ RosegardenMainWindow::slotRelabelSegments()
 
     bool ok = false;
 
-    QString newLabel = InputDialog::getText(dynamic_cast<QWidget*>(this), QString("Input"), tr("Enter new label"));
+    QString newLabel = InputDialog::getText(dynamic_cast<QWidget*>(this), QString("Input"), tr("Enter new label"), LineEdit::Normal, QString(), &ok);
 
     if (ok) {
         CommandHistory::getInstance()->addCommand
