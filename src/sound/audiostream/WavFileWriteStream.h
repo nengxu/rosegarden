09473e19faca4bf3ee0418389823f8b7a9a22f0d
cannam <cannam@cf6a83e7-f017-4305-b0b3-678bde24f230>
2010-02-08 11:55:32 +0000

    * add note on authorship to audiostream files -- I use this code elsewhere   so would prefer it to be clear that it originated with me rather than my   having pulled it from RG
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@11792 cf6a83e7-f017-4305-b0b3-678bde24f230

--- src/sound/audiostream/WavFileWriteStream.h
+++ src/sound/audiostream/WavFileWriteStream.h
@@ -5,6 +5,8 @@
     A MIDI and audio sequencer and musical notation editor.
     Copyright 2000-2010 the Rosegarden development team.
  
+    This file is Copyright 2005-2010 Chris Cannam.
+
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License as
     published by the Free Software Foundation; either version 2 of the
