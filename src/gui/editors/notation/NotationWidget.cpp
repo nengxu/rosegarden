e1e851442fd42c6cdf43ed1fc52ab06bd4969a05
dmmcintyr <dmmcintyr@cf6a83e7-f017-4305-b0b3-678bde24f230>
2011-01-04 17:52:58 +0000

    Revert the patch from Alvar Udras as suggested by John Anderson.  I've looked back and tried to figure out what behavior Alvar was trying to cure with that patch, and I don't really understand what the patch was meant to do.  John is completely correct that the patch was responsible for the notation editor's failure to scroll while moving the cursor around with various edit keys. Reverting this patch clearly has high value in terms of usability improvement, but I would be more comfortable if I understood what compromise I'm accepting in so doing.  This leaves some other bad behavior lurking out there, to be rediscovered one day, and at that time, hopefully we can arrive at a better solution to that other problem than this one turned out to be.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@12180 cf6a83e7-f017-4305-b0b3-678bde24f230

--- src/gui/editors/notation/NotationWidget.cpp
+++ src/gui/editors/notation/NotationWidget.cpp
@@ -867,7 +867,7 @@ NotationWidget::slotPointerPositionChanged(timeT t, bool moveView)
         m_hpanner->slotShowPositionPointer(QPointF(sceneX, sceneY), height);
     }
 
-    if ((getPlayTracking() && (m_document->getSequenceManager()->getTransportStatus() != STOPPED )) || !fromDocument) {
+    if (getPlayTracking() || !fromDocument) {
         if (moveView) m_view->slotEnsurePositionPointerInView(fromDocument);
     }
 }
