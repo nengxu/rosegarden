2bd6d78da66d1e0c3d7abc88739c601dc0102763
dmmcintyr <dmmcintyr@cf6a83e7-f017-4305-b0b3-678bde24f230>
2011-09-26 02:27:38 +0000

    Fixing the broken reversion point... (individual messages save me having to fish out moderated bugs list posts)
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@12570 cf6a83e7-f017-4305-b0b3-678bde24f230

--- data/locale/ca.ts
+++ data/locale/ca.ts
@@ -23863,8 +23863,13 @@ Voleu configurar-ho ara?</translation>
         <translation type="unfinished">Converteix els fragments repetits en còpies</translation>
     </message>
     <message>
-        <location line="+26"/>
-        <source>Turn %n Linked Segment(s) into Real Copies</source>
+        <location line="+29"/>
+        <source>Turn Linked Segment into Real Copies</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Turn Linked Segments into Real Copies</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
