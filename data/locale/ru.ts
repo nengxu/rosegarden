c632b9ed6e49f515387d1debe722bce4b827cd6e
dmmcintyr <dmmcintyr@cf6a83e7-f017-4305-b0b3-678bde24f230>
2011-09-26 02:25:22 +0000

    Individual translation update to avoid flooding...
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@12549 cf6a83e7-f017-4305-b0b3-678bde24f230

--- data/locale/ru.ts
+++ data/locale/ru.ts
@@ -22857,13 +22857,8 @@ Would you like to set it now?</source>
         <translation>Превратить повторение сегментов в их настоящие копии</translation>
     </message>
     <message>
-        <location line="+29"/>
-        <source>Turn Linked Segment into Real Copies</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location line="+2"/>
-        <source>Turn Linked Segments into Real Copies</source>
+        <location line="+26"/>
+        <source>Turn %n Linked Segment(s) into Real Copies</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
