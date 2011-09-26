b44ae1bd1706b9467518350f5d79be6104d14c67
dmmcintyr <dmmcintyr@cf6a83e7-f017-4305-b0b3-678bde24f230>
2011-09-26 02:26:42 +0000

    Fixing the broken reversion point... (individual messages save me having to fish out moderated bugs list posts)
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@12557 cf6a83e7-f017-4305-b0b3-678bde24f230

--- data/locale/zh_CN.ts
+++ data/locale/zh_CN.ts
@@ -23768,8 +23768,13 @@ Would you like to set it now?</source>
         <translation>将重复的片段转换为真实副本</translation>
     </message>
     <message>
-        <location line="+26"/>
-        <source>Turn %n Linked Segment(s) into Real Copies</source>
+        <location line="+29"/>
+        <source>Turn Linked Segment into Real Copies</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Turn Linked Segments into Real Copies</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
