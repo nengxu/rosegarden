c9e42b0a5490764b01744a0c1c50a7985173313e
dmmcintyr <dmmcintyr@cf6a83e7-f017-4305-b0b3-678bde24f230>
2011-09-26 02:38:41 +0000

    Repair translation (commit file by file to avoid huge diffs)
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@12576 cf6a83e7-f017-4305-b0b3-678bde24f230

--- data/locale/ja.ts
+++ data/locale/ja.ts
@@ -22805,15 +22805,12 @@ Would you like to set it now?</source>
         <source>Turn Repeating Segments into Real Copies</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <location line="+29"/>
-        <source>Turn Linked Segment into Real Copies</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location line="+2"/>
-        <source>Turn Linked Segments into Real Copies</source>
-        <translation type="unfinished"></translation>
+    <message numerus="yes">
+        <location line="+25"/>
+        <source>Turn %n Linked Segment(s) into Real Copies</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
     </message>
     <message>
         <location line="+230"/>
