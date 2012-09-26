65052555047d90230acd5c288dfde42403149c6e
tedfelix <tedfelix@cf6a83e7-f017-4305-b0b3-678bde24f230>
2012-09-26 22:26:02 +0000

    Comments about using autoreconf.
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@13105 cf6a83e7-f017-4305-b0b3-678bde24f230

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-aclocal -I . && autoconf 
+
+# This script creates the configure script using the autoconf tools.
+
+# A more modern and complete way of doing this:
+#
+#   autoreconf --force --install
+#
+# We might want to switch to this at some point in the future.
+
+aclocal -I . && autoconf
+
