26558bd63b3e1547f8c7cf5384c9344b94ed3617
tedfelix <tedfelix@cf6a83e7-f017-4305-b0b3-678bde24f230>
2013-08-01 13:46:31 +0000

    Add new NotationPreview class.
    This is a work in progress.  Committing it for a backup.  I'm trying to
    pull the notation preview concept out of CompositionModelImpl.  This
    is one of the areas that uses excessive CPU time.  Isolating it will
    make it easier to swap it out with a more efficient implementation.
    
    At this point, NotationPreview is unused code, so it should have no 
    effect on the system.
    
    
    git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@13346 cf6a83e7-f017-4305-b0b3-678bde24f230

---
+++ src/gui/editors/segment/compositionview/NotationPreview.cpp
@@ -0,0 +1,268 @@
+/* -*- c-basic-offset: 4 indent-tabs-mode: nil -*- vi:set ts=8 sts=4 sw=4: */
+
+/*
+    Rosegarden
+    A MIDI and audio sequencer and musical notation editor.
+    Copyright 2000-2013 the Rosegarden development team.
+ 
+    Other copyrights also apply to some parts of this work.  Please
+    see the AUTHORS file and individual file headers for details.
+ 
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version.  See the file
+    COPYING included with this distribution for more information.
+*/
+
+#define RG_MODULE_STRING "[NotationPreview]"
+
+// Local
+#include "NotationPreview.h"
+
+// Rosegarden
+#include "base/Composition.h"
+#include "base/Studio.h"
+#include "base/BaseProperties.h"
+
+// System
+#include <math.h>
+#include <algorithm>
+
+namespace Rosegarden
+{
+
+
+NotationPreview::NotationPreview(Composition &composition,
+                                 Studio &studio,
+                                 RulerScale *rulerScale,
+                                 int vStep) :
+    m_composition(composition),
+    m_studio(studio),
+    m_grid(rulerScale, vStep)
+{
+}
+
+#if 0
+// Almost builds.  Just a few properties are needed.
+void NotationPreview::makeNotationPreviewRects(QPoint basePoint,
+        const Segment* segment, const QRect& clipRect, RectRanges* npRects)
+{
+    Profiler profiler("NotationPreview::makeNotationPreviewRects");
+
+    RectList* cachedNPData = getNotationPreviewData(segment);
+
+    if (cachedNPData->empty())
+        return ;
+
+    RectList::iterator npEnd = cachedNPData->end();
+
+    // Find the first preview rect that *starts within* the clipRect.
+    // Probably not the right thing to do as this means any event that starts
+    // prior to the clipRect but stretches through the clipRect will be
+    // dropped.  And this explains why long notes disappear from the segment
+    // previews.
+    // Note that RectList is a std::vector, so this call will take increasing
+    // amounts of time as the number of events to the left of the clipRect
+    // increases.  This is probably at least a small part of the "CPU usage
+    // increasing over time" issue.
+    // If cachedNPData is sorted by start time, we could at least do a binary
+    // search.
+    RectList::iterator npi = std::lower_bound(cachedNPData->begin(), npEnd, clipRect, RectCompare());
+
+    // If no preview rects were within the clipRect, bail.
+    if (npi == npEnd)
+        return ;
+
+    // ??? Go back one event if we aren't already at the beginning.  Why?
+    // Hilariously, this partially "fixes" the "missing event in preview"
+    // problem.  However, it only "fixes" the problem for a single event.
+    // Is that why this is here?
+    // When testing, to get around the fact that the segments are drawn on a
+    // segment layer in CompositionView, just disable then re-enable segment
+    // previews in the menu and the "missing event in preview" problem is easy
+    // to see.
+    if (npi != cachedNPData->begin())
+        --npi;
+
+    // Compute the interval within the Notation Preview for this segment.
+
+    RectRange interval;
+    interval.range.first = npi;
+
+    // Compute the rightmost x coord (xLim)
+    int segEndX = int(nearbyint(m_grid.getRulerScale()->getXForTime(segment->getEndMarkerTime())));
+    int xLim = std::min(clipRect.right(), segEndX);
+
+    //RG_DEBUG << "NotationPreview::makeNotationPreviewRects : basePoint.x : "
+    //         << basePoint.x();
+
+    // Search sequentially for the last preview rect in the segment.
+    while (npi != npEnd  &&  npi->x() < xLim)
+        ++npi;
+
+    interval.range.second = npi;
+    interval.basePoint.setX(0);
+    interval.basePoint.setY(basePoint.y());
+    interval.color = computeSegmentPreviewColor(segment);
+
+    // Add the interval to the caller's interval list.
+    npRects->push_back(interval);
+}
+
+void NotationPreview::makeNotationPreviewRectsMovingSegment(QPoint basePoint,
+        const Segment* segment, const QRect& currentSR, RectRanges* npRects)
+{
+    CompositionRect unmovedSR = computeSegmentRect(*segment);
+
+    RectList* cachedNPData = getNotationPreviewData(segment);
+
+    if (cachedNPData->empty())
+        return ;
+
+    RectList::iterator npBegin = cachedNPData->begin();
+    RectList::iterator npEnd = cachedNPData->end();
+
+    RectList::iterator npi;
+
+    if (getChangeType() == ChangeResizeFromStart)
+        npi = std::lower_bound(npBegin, npEnd, currentSR, RectCompare());
+    else
+        npi = std::lower_bound(npBegin, npEnd, unmovedSR, RectCompare());
+
+    if (npi == npEnd)
+        return ;
+
+    // ??? Bump iterator back one to try and pick up the previous event
+    //     rectangle which might be needed.
+    if (npi != npBegin  &&  getChangeType() != ChangeResizeFromStart) {
+        --npi;
+    }
+
+    // Compute the interval within the Notation Preview for this segment.
+
+    RectRange interval;
+    interval.range.first = npi;
+
+    // Compute the rightmost x coord (xLim)
+    int xLim = getChangeType() == ChangeMove ? unmovedSR.right() : currentSR.right();
+
+    //RG_DEBUG << "NotationPreview::makeNotationPreviewRectsMovingSegment : basePoint.x : "
+    //         << basePoint.x();
+
+    // Search sequentially for the last preview rect in the segment.
+    while (npi != npEnd  &&  npi->x() < xLim)
+        ++npi;
+
+    interval.range.second = npi;
+    interval.basePoint.setY(basePoint.y());
+
+    if (getChangeType() == ChangeMove)
+        interval.basePoint.setX(basePoint.x() - unmovedSR.x());
+    else
+        interval.basePoint.setX(0);
+
+    interval.color = computeSegmentPreviewColor(segment);
+
+    npRects->push_back(interval);
+}
+#endif
+
+NotationPreview::RectList* NotationPreview::getNotationPreviewData(const Segment* s)
+{
+    RectList* npData = m_notationPreviewDataCache[s];
+
+    if (!npData) {
+        npData = makeNotationPreviewDataCache(s);
+    }
+
+    return npData;
+}
+
+NotationPreview::RectList* NotationPreview::makeNotationPreviewDataCache(const Segment *s)
+{
+    RectList* npData = new RectList();
+
+    // Create the preview
+    createEventRects(s, npData);
+
+    // Store in the cache.
+    // Callers guarantee that m_notationPreviewDataCache[s] is not currently
+    // pointing to anything.
+    m_notationPreviewDataCache[s] = npData;
+
+    return npData;
+}
+
+void NotationPreview::createEventRects(const Segment *segment, RectList *npData)
+{
+    npData->clear();
+
+    int segStartX = static_cast<int>(nearbyint(
+            m_grid.getRulerScale()->getXForTime(segment->getStartTime())));
+
+    bool isPercussion = false;
+    Track *track = m_composition.getTrackById(segment->getTrack());
+    if (track) {
+        InstrumentId iid = track->getInstrument();
+        Instrument *instrument = m_studio.getInstrumentById(iid);
+        if (instrument && instrument->isPercussion()) isPercussion = true;
+    }
+
+    // For each event in the segment
+    for (Segment::const_iterator i = segment->begin();
+         i != segment->end(); ++i) {
+
+        long pitch = 0;
+        if (!(*i)->isa(Note::EventType) ||
+            !(*i)->get<Int>(BaseProperties::PITCH, pitch)) {
+            continue;
+        }
+
+        timeT eventStart = (*i)->getAbsoluteTime();
+        timeT eventEnd = eventStart + (*i)->getDuration();
+        //  if (eventEnd > segment->getEndMarkerTime()) {
+        //      eventEnd = segment->getEndMarkerTime();
+        //  }
+
+        int x = static_cast<int>(nearbyint(
+                m_grid.getRulerScale()->getXForTime(eventStart)));
+        int width = static_cast<int>(nearbyint(
+                m_grid.getRulerScale()->getWidthForDuration(
+                        eventStart, eventEnd - eventStart)));
+
+        //RG_DEBUG << "NotationPreview::createEventRects: x = " << x << ", width = " << width << " (time = " << eventStart << ", duration = " << eventEnd - eventStart << ")";
+
+        if (x <= segStartX) {
+            ++x;
+            if (width > 1) --width;
+        }
+        if (width > 1) --width;
+        if (width < 1) ++width;
+
+        const double y0 = 0;
+        const double y1 = m_grid.getYSnap();
+        double y = y1 + ((y0 - y1) * (pitch - 16)) / 96;
+
+        int height = 1;
+
+        if (isPercussion) {
+            height = 2;
+            if (width > 2) width = 2;
+        }
+
+        if (y < y0) y = y0;
+        if (y > y1 - height + 1) y = y1 - height + 1;
+
+        // ??? static_cast<int>(nearbyint(y))?
+        QRect r(x, static_cast<int>(y), width, height);
+
+        npData->push_back(r);
+    }
+}
+
+
+}
+
+// We're not a QObject just yet.
+//#include "NotationPreview.moc"
---
+++ src/gui/editors/segment/compositionview/NotationPreview.h
@@ -0,0 +1,147 @@
+/* -*- c-basic-offset: 4 indent-tabs-mode: nil -*- vi:set ts=8 sts=4 sw=4: */
+
+/*
+    Rosegarden
+    A MIDI and audio sequencer and musical notation editor.
+    Copyright 2000-2013 the Rosegarden development team.
+
+    Other copyrights also apply to some parts of this work.  Please
+    see the AUTHORS file and individual file headers for details.
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version.  See the file
+    COPYING included with this distribution for more information.
+*/
+
+#ifndef RG_NOTATIONPREVIEW_H
+#define RG_NOTATIONPREVIEW_H
+
+// Rosegarden
+#include "base/SnapGrid.h"
+
+// Qt
+#include <QRect>
+#include <QColor>
+
+// System
+#include <map>
+#include <vector>
+
+namespace Rosegarden
+{
+
+
+// Forward declares
+class Composition;
+class Studio;
+class Segment;
+
+/// Draw Event objects on a QPainter
+/**
+ * WORK IN PROGRESS
+ *
+ * This code is not used by any part of the system yet.  I am working on
+ * pulling the notation preview code out of CompositionModelImpl.  This
+ * will provide a class for an important concept.  And that's really what
+ * it's all about.
+ *
+ * TODO
+ * - Finish bringing in all necessary pieces.
+ * - Evaluate all use of m_notationPreviewDataCache in CompositionModelImpl.
+ * - Come up with a clever way to swap this in without disturbing anything
+ *   so we can easily go back to the old way.
+ *
+ * We should probably be heading towards a MIDISegmentView class that renders
+ * a segment (or all the segments in a composition) on a QPainter.
+ *
+ * One key question is whether this class
+ * should handle a single segment, or all the segments in the composition.
+ * Not sure which results in the simpler implementation.  Handling a single
+ * segment means the client would need to manage a collection of these
+ * objects.  Handling all segments means it would need to juggle a cache
+ * for each segment, much like the current implementation.  Maybe we could
+ * do something in between.  Perhaps it could look like it handles all of
+ * the segments, but internally it will manage "info" structs for each
+ * segment.  This really doesn't have the same issues as other systems I've
+ * worked on, so I'm not sure that the one-for-one approach has any benefit.
+ *
+ * Let's go with managing all segments first as that is in keeping with the
+ * code it is coming from.  Then we'll think through whether pulling out
+ * the "aggregation" has any benefits.
+ */
+class NotationPreview
+{
+public:
+    NotationPreview(Composition &composition,
+                    Studio &studio,
+                    RulerScale *rulerScale,
+                    int vStep);
+
+    typedef std::vector<QRect> RectList;
+
+    struct RectRange {
+        std::pair<RectList::iterator, RectList::iterator> range;
+        QPoint basePoint;
+        QColor color;
+    };
+
+    typedef std::vector<RectRange> RectRanges;
+
+    // From CompositionView::drawSegments().
+    // ??? Should this class be able to draw itself?
+    //     This would mean that CompositionView could own the object.
+    //     I think that in the end this is indeed the right thing to do.
+    //     NotationPreview is a "view" class.  It is a view of the
+    //     events in a segment drawn on a QPainter.  I guess it is really
+    //     part of a SegmentView.  So maybe we should eventually have a
+    //     MIDISegmentView and an AudioSegmentView.  CompositionView would
+    //     bring these together to draw the composition.
+    void draw();
+
+    // From CompositionModelImpl::getSegmentRects()
+    // ??? Or should it just provide this and let CompositionView draw?
+    //     This would mean that CompositionModelImpl might own the object.
+    //     Probably easiest to go with this first, then see if moving
+    //     draw into here is possible and whether it helps simplify things.
+    void getSegmentRects();
+
+private:
+    Composition &m_composition;
+    Studio &m_studio;
+    SnapGrid m_grid;
+
+    struct RectCompare {
+        bool operator()(const QRect &r1, const QRect &r2) const {
+            return r1.x() < r2.x();
+        }
+    };
+
+    // From CompositionModelImpl::makeNotationPreviewRects()
+    void makeNotationPreviewRects(QPoint basePoint,
+            const Segment* segment, const QRect& clipRect, RectRanges* npRects);
+
+    // From CompositionModelImpl::makeNotationPreviewRectsMovingSegment()
+    void makeNotationPreviewRectsMovingSegment(QPoint basePoint,
+            const Segment* segment, const QRect& currentSR, RectRanges* npRects);
+
+    // From CompositionModelImpl::getNotationPreviewData()
+    RectList *getNotationPreviewData(const Segment* s);
+
+    // From CompositionModelImpl::makeNotationPreviewDataCache()
+    RectList *makeNotationPreviewDataCache(const Segment *s);
+
+    // From CompositionModelImpl::createEventRects()
+    void createEventRects(const Segment *segment, RectList *rects);
+
+    typedef std::map<const Segment *, RectList *> NotationPreviewDataCache;
+    // ??? Need to bring in all code from CompositionModelImpl that maintains
+    //     this cache.
+    NotationPreviewDataCache m_notationPreviewDataCache;
+};
+
+
+}
+
+#endif
