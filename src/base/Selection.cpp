nengxu/rosegarden

commit d5dd1d32c25eb2f7d3171d882e3f9b10615942b2

Fix #1410.  There was a sanity check, but no return statement, so it marched blithely along and crashed.  The particular circumstances that trigger this sanity check have to do with pasting a rest at the very end of a segment. After making the sanity check work, the rest in question is not selected when it was expected to be; apparently because the actual rest at the end is different from the one we tried to paste.  Probably not worth sorting out, since it has taken years to discover this omission.  Avoid crashing and be happy.
git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@13450 cf6a83e7-f017-4305-b0b3-678bde24f230

src/base/Selection.cpp
@@ -169,7 +169,7 @@ EventSelection::addRemoveEvent(Event *e, EventFuncPtr insertEraseFn,
     if (baseSegmentItr == m_originalSegment.end()) {
         RG_DEBUG << "EventSelection::addRemoveEvent Sent event that can not be found "
                   << "in segment.  Exiting method";
-    
+        return;    
     }
 
     timeT eventDuration = e->getDuration();

commit b57d80008c877f15a870790ca18a24e4ed9937e8

Fix crashes caused by r13450
The added return was preventing the event from being removed from the
selection.  Consequently the selection contained a pointer to a deleted
event.

Also added a skeletal EventSelection::dump() for future use.  Some minor
formatting and comments.


git-svn-id: http://svn.code.sf.net/p/rosegarden/code/trunk/rosegarden@13509 cf6a83e7-f017-4305-b0b3-678bde24f230

src/base/Selection.cpp
@@ -165,12 +165,13 @@ EventSelection::addRemoveEvent(Event *e, EventFuncPtr insertEraseFn,
 {
     const Segment::const_iterator baseSegmentItr = m_originalSegment.find(e);
     
-    // Sanity Check
-    if (baseSegmentItr == m_originalSegment.end()) {
-        RG_DEBUG << "EventSelection::addRemoveEvent Sent event that can not be found "
-                  << "in segment.  Exiting method";
-        return;    
-    }
+    //if (baseSegmentItr == m_originalSegment.end()) {
+    //    RG_DEBUG << "EventSelection::addRemoveEvent(): "
+    //             << "Sent event that can not be found in original segment.";
+        // Note: This is perfectly ok.  The rest of the code checks
+        //       baseSegmentIter to make sure it is valid before using
+        //       it.
+    //}
 
     timeT eventDuration = e->getDuration();
     if (eventDuration == 0) eventDuration = 1;
@@ -451,6 +452,18 @@ EventSelection::segmentDeleted(const Segment *)
               */
 }
 
+void
+EventSelection::dump() const
+{
+    RG_DEBUG << "EventSelection::dump()";
+    RG_DEBUG << "  m_beginTime: " << m_beginTime;
+    RG_DEBUG << "  m_endTime: " << m_endTime;
+    RG_DEBUG << "  m_haveRealStartTime: " << m_haveRealStartTime;
+}
+
+
+/** SegmentSelection **/
+
 bool SegmentSelection::hasNonAudioSegment() const
 {
     for (const_iterator i = begin(); i != end(); ++i) {
@@ -459,6 +472,7 @@ bool SegmentSelection::hasNonAudioSegment() const
     }
     return false;
 }
+
 
 /** Templates that define methods of TimewiseSelection **/
 
@@ -541,6 +555,7 @@ TimeSignatureSelection::AddToComposition(Composition *composition)
     }    
 }
 
+
 /** Methods of TempoSelection **/
 
 TempoSelection::TempoSelection() { }
@@ -612,6 +627,7 @@ TempoSelection::AddToComposition(Composition *composition)
     }
 }
 
+
 /** Methods of template helper MarkerElementInfo **/
 
 void
@@ -633,6 +649,7 @@ AddToComposition(Composition *composition,
 // Explicit template instantiation, so other cpp files don't need to
 // see the method definitions.
 template class TimewiseSelection<MarkerElementInfo>;
+
 
 /** Methods of MarkerSelection **/
 

src/base/Selection.h
@@ -165,6 +165,9 @@ public:
     virtual void endMarkerTimeChanged(const Segment *, bool) { }
     virtual void segmentDeleted(const Segment *);
     
+    // Debug
+    void dump() const;
+
 private:
     EventSelection &operator=(const EventSelection &);
   

src/gui/editors/notation/NotationView.cpp
@@ -4756,6 +4756,7 @@ NotationView::slotTransformsNormalizeRests()
 
     if (!selection)
         return ;
+
     TmpStatusMsg msg(tr("Normalizing rests..."), this);
 
     CommandHistory::getInstance()->